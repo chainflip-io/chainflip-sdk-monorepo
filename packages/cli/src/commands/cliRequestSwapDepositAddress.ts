9ab6bb5feaa06ba1108b2359affd84dc106d379c
Andrew Dibble <andrew@chainflip.io>
2023-07-18 14:03:14 +0200

    add cli command

---
+++ packages/cli/src/commands/cliRequestSwapDepositAddress.ts
@@ -0,0 +1,49 @@
+import { ArgumentsCamelCase, InferredOptionTypes, Options } from 'yargs';
+import { Assets, Chains } from '@/shared/enums';
+import { BrokerClient } from '../lib';
+
+export const yargsOptions = {
+  'src-asset': {
+    choices: Object.values(Assets),
+    describe: 'The asset to swap from',
+    demandOption: true,
+  },
+  'dest-asset': {
+    choices: Object.values(Assets),
+    demandOption: true,
+    describe: 'The asset to swap to',
+  },
+  'dest-address': {
+    type: 'string',
+    demandOption: true,
+    describe: 'The address to send the swapped assets to',
+  },
+  'broker-url': {
+    type: 'string',
+    describe: 'The broker URL',
+  },
+  'src-chain': {
+    choices: Object.values(Chains),
+    describe: 'The chain to swap from',
+    demandOption: true,
+  },
+} as const satisfies { [key: string]: Options };
+
+export default async function cliRequestSwapDepositAddress(
+  args: ArgumentsCamelCase<InferredOptionTypes<typeof yargsOptions>>,
+) {
+  const client = await BrokerClient.create({ url: args.brokerUrl });
+
+  const result = await client.requestSwapDepositAddress({
+    srcAsset: args.srcAsset,
+    destAsset: args.destAsset,
+    destAddress: args.destAddress,
+    srcChain: args.srcChain,
+  });
+
+  console.log(`Deposit address: ${result.address}`);
+  console.log(`Issued block: ${result.issuedBlock}`);
+  console.log(`Expiry block: ${result.expiryBlock}`);
+
+  await client.close();
+}
