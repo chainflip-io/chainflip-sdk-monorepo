5d61b5f7206b888a0afa5b3a674b94a22e44a04a
Andrew Dibble <andrew@chainflip.io>
2023-08-22 10:46:01 +0200

    remove import

--- packages/shared/src/node-apis/__tests__/RpcClient.test.ts
+++ packages/shared/src/node-apis/__tests__/RpcClient.test.ts
@@ -1,4 +1,4 @@
-import { on, once } from 'events';
+import { once } from 'events';
 import { AddressInfo, WebSocketServer } from 'ws';
 import { z } from 'zod';
 import RpcClient from '../RpcClient';
