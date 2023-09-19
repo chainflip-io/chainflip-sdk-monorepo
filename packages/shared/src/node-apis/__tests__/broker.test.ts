c8b195a916fb587df98ede17063df242f82c1d22
Niklas Natter <nat@chainflip.io>
2023-09-19 15:36:21 +0200

    fix: Pass gas budget to broker as hex encoded number (#225)

--- packages/shared/src/node-apis/__tests__/broker.test.ts
+++ packages/shared/src/node-apis/__tests__/broker.test.ts
@@ -83,7 +83,7 @@ describe(BrokerClient.prototype.requestSwapDepositAddress, () => {
       destAddress: '0xcafebabe',
       destChain: 'Ethereum',
       ccmMetadata: {
-        gasBudget: '123',
+        gasBudget: '123456789',
         message: '0xdeadc0de',
       },
     });
@@ -115,7 +115,7 @@ describe(BrokerClient.prototype.requestSwapDepositAddress, () => {
         '0xcafebabe',
         0,
         {
-          gas_budget: '123',
+          gas_budget: '0x75bcd15',
           message: '0xdeadc0de',
         },
       ],
