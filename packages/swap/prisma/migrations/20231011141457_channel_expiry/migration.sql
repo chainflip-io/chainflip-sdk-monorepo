a725a3b43a367d5421c4980d57a04e0c3a9e0224
Andrew Dibble <andrew@chainflip.io>
2023-10-11 16:31:07 +0200

    update migration

--- packages/swap/prisma/migrations/20231011141457_channel_expiry/migration.sql
+++ packages/swap/prisma/migrations/20231011141457_channel_expiry/migration.sql
@@ -1,7 +1,4 @@
 -- AlterTable
 ALTER TABLE "public"."SwapDepositChannel" ADD COLUMN     "expiryBlock" INTEGER,
-ADD COLUMN     "isExpired" BOOLEAN NOT NULL DEFAULT true,
+ADD COLUMN     "isExpired" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "srcChainExpiryBlock" BIGINT;
-
-ALTER TABLE "public"."SwapDepositChannel"
-ALTER COLUMN "isExpired" SET DEFAULT false;
