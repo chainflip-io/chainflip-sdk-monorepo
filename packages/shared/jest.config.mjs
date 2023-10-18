d3c684f51f005af93eb26888ddf46fd4a2ef3fcf
Andrew Dibble <andrew@dibble.codes>
2023-10-18 17:21:36 +0200

    duh

--- packages/shared/jest.config.mjs
+++ packages/shared/jest.config.mjs
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/no-relative-packages
 import baseConfig from '../../jest.config.mjs';
 
-process.env.RPC_BROKER_WSS_URL = 'wss://rpc-broker.example.com';
+process.env.RPC_BROKER_HTTPS_URL = 'wss://rpc-broker.example.com';
 
 export default {
   ...baseConfig,
