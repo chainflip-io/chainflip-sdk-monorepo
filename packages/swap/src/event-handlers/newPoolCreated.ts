91835b8014795e3312635f39627614f1c3326c8a
Niklas Natter <nat@chainflip.io>
2023-11-03 11:44:47 +0100

    fix: Support previous shape of NewPoolCreated event to index sisyphos and perseverance (#307)
    * fix: Support previous shape of NewPoolCreated event to index sisyphos and perseverance
    
    * Update packages/swap/src/event-handlers/newPoolCreated.ts

--- packages/swap/src/event-handlers/newPoolCreated.ts
+++ packages/swap/src/event-handlers/newPoolCreated.ts
@@ -2,11 +2,24 @@ import { z } from 'zod';
 import { chainflipAssetEnum, unsignedInteger } from '@/shared/parsers';
 import type { EventHandlerArgs } from './index';
 
-const eventArgs = z.object({
-  baseAsset: chainflipAssetEnum,
-  pairAsset: chainflipAssetEnum,
-  feeHundredthPips: unsignedInteger,
-});
+const eventArgs = z.union([
+  z.object({
+    baseAsset: chainflipAssetEnum,
+    pairAsset: chainflipAssetEnum,
+    feeHundredthPips: unsignedInteger,
+  }),
+  // support previous event shape used on sisyphos 0.9
+  z
+    .object({
+      unstableAsset: chainflipAssetEnum,
+      feeHundredthPips: unsignedInteger,
+    })
+    .transform(({ unstableAsset, feeHundredthPips }) => ({
+      baseAsset: 'USDC' as const,
+      pairAsset: unstableAsset,
+      feeHundredthPips,
+    })),
+]);
 
 export default async function newPoolCreated({
   prisma,
