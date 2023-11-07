dd551371f7ac5896c2758f58062a9629cc25a951
Zoheb Salam <zoheb@chainflip.io>
2023-11-07 18:05:27 +0100

    use trpc for get status

--- packages/swap/src/server.ts
+++ packages/swap/src/server.ts
@@ -3,19 +3,27 @@ import cors from 'cors';
 import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-import { openSwapDepositChannelSchema } from '@/shared/schemas';
+import {
+  getSwapStatusSchema,
+  openSwapDepositChannelSchema,
+  swapResponseSchema,
+} from '@/shared/schemas';
+import getSwapStatus from './handlers/getSwapStatus';
 import openSwapDepositChannel from './handlers/openSwapDepositChannel';
 import authenticate from './quoting/authenticate';
 import fee from './routes/fee';
 import quote from './routes/quote';
-import swap from './routes/swap';
 import thirdPartySwap from './routes/thirdPartySwap';
 import { publicProcedure, router } from './trpc';
 
-const appRouter = router({
+export const appRouter = router({
   openSwapDepositChannel: publicProcedure
     .input(openSwapDepositChannelSchema)
     .mutation((v) => openSwapDepositChannel(v.input)),
+  getStatus: publicProcedure
+    .input(getSwapStatusSchema)
+    .output(swapResponseSchema)
+    .query((v) => getSwapStatus(v.input)),
 });
 
 export type AppRouter = typeof appRouter;
@@ -25,7 +33,6 @@ const server = createServer(app);
 const io = new Server(server).use(authenticate);
 
 app.use('/fees', fee);
-app.use('/swaps', express.json(), swap);
 app.use('/third-party-swap', express.json(), thirdPartySwap);
 
 app.get('/healthcheck', (req, res) => {
