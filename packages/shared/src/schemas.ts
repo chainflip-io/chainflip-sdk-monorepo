359ff5731f5a86477b92eb583f5833c60a6ac66b
Andrew Dibble <andrew@chainflip.io>
2023-11-21 13:08:02 +0100

    chore(sdk): revert
    This reverts commit cd767a3c1f088be1e86a63503908243c755669b5.

--- packages/shared/src/schemas.ts
+++ packages/shared/src/schemas.ts
@@ -5,11 +5,8 @@ import {
   chainflipAssetAndChain,
   chainflipChain,
   hexStringWithMaxByteSize,
-  number,
   numericString,
-  string,
 } from './parsers';
-import { channelIdRegex } from './strings';
 
 export const quoteQuerySchema = z.object({
   srcAsset: chainflipAssetAndChain,
@@ -41,104 +38,6 @@ export const openSwapDepositChannelSchema = z
     ...rest,
     expectedDepositAmount: amount,
   }));
-
-export const depositChannelIdRegex = z.string().regex(channelIdRegex);
-export const swapIdRegex = z.string().regex(/^\d+$/i);
-export const txHashRegex = z.string().regex(/^0x[a-f\d]+$/i);
-
-const swapStatusId = z.union([depositChannelIdRegex, swapIdRegex, txHashRegex]);
-
-export const getSwapStatusSchema = z.object({
-  id: swapStatusId,
-});
-
-export const swapState = z.enum([
-  'AWAITING_DEPOSIT',
-  'DEPOSIT_RECEIVED',
-  'SWAP_EXECUTED',
-  'EGRESS_SCHEDULED',
-  'BROADCAST_REQUESTED',
-  'BROADCAST_ABORTED',
-  'COMPLETE',
-]);
-
-const baseSwapResponseSchema = z.object({
-  srcChain: chainflipChain,
-  destChain: chainflipChain,
-  srcAsset: chainflipAsset,
-  destAsset: chainflipAsset,
-  destAddress: string.optional(),
-  depositAddress: string.optional(),
-  depositChannelCreatedAt: number.optional(),
-  expectedDepositAmount: string.optional(),
-  depositChannelExpiryBlock: z.bigint().optional().nullable(),
-  estimatedDepositChannelExpiryTime: number.optional(),
-  isDepositChannelExpired: z.boolean(),
-  ccmDepositReceivedBlockIndex: string.optional().nullable(),
-  ccmMetadata: z
-    .object({
-      gasBudget: string,
-      message: string.nullable(),
-    })
-    .optional()
-    .nullable(),
-});
-
-const awaitingState = baseSwapResponseSchema.extend({
-  state: swapState.refine((state) => state === swapState.Enum.AWAITING_DEPOSIT),
-  depositAmount: string.optional(),
-  depositTransactionHash: string.optional(),
-  depositTransactionConfirmations: number.optional(),
-});
-const depositReceivedState = awaitingState.extend({
-  state: swapState.refine((state) => state === swapState.Enum.DEPOSIT_RECEIVED),
-  swapId: string,
-  depositAmount: string,
-  depositReceivedAt: number,
-  depositReceivedBlockIndex: string,
-});
-const swapExecutedState = depositReceivedState.extend({
-  state: swapState.refine((state) => state === swapState.Enum.SWAP_EXECUTED),
-  intermediateAmount: string.optional(),
-  swapExecutedAt: number,
-  swapExecutedBlockIndex: string,
-});
-const egressScheduledState = swapExecutedState.extend({
-  state: swapState.refine((state) => state === swapState.Enum.EGRESS_SCHEDULED),
-  egressAmount: string,
-  egressScheduledAt: number,
-  egressScheduledBlockIndex: string,
-});
-const broadcastRequestedState = egressScheduledState.extend({
-  state: swapState.refine(
-    (state) => state === swapState.Enum.BROADCAST_REQUESTED,
-  ),
-  broadcastRequestedAt: number,
-  broadcastRequestedBlockIndex: string,
-});
-const broadcastAbortedState = broadcastRequestedState.extend({
-  state: swapState.refine(
-    (state) => state === swapState.Enum.BROADCAST_ABORTED,
-  ),
-  broadcastAbortedAt: number,
-  broadcastAbortedBlockIndex: string,
-});
-const broadcastSucceededState = broadcastRequestedState.extend({
-  state: swapState.refine((state) => state === swapState.Enum.COMPLETE),
-  swapExecutedBlockIndex: string,
-  broadcastSucceededAt: number,
-  broadcastSucceededBlockIndex: string,
-});
-
-export const swapResponseSchema = z.union([
-  awaitingState,
-  depositReceivedState,
-  swapExecutedState,
-  egressScheduledState,
-  broadcastRequestedState,
-  broadcastAbortedState,
-  broadcastSucceededState,
-]);
 
 export type OpenSwapDepositChannelArgs = z.input<
   typeof openSwapDepositChannelSchema
