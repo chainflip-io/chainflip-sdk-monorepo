68ff4ca912b28affa552efb6ac7268d3332ea428
Niklas Natter <nat@chainflip.io>
2023-12-07 17:33:11 +0100

    chore(swap): Always use unstable asset as base asset to make pools table consistent with backend (#383)
    * chore(swap): Always store unstable asset as base asset
    
    * Fix accessing database
    
    * Support old event shape in poolFeeSet

--- packages/swap/src/event-handlers/newPoolCreated.ts
+++ packages/swap/src/event-handlers/newPoolCreated.ts
@@ -2,46 +2,53 @@ import { z } from 'zod';
 import { chainflipAssetEnum, unsignedInteger } from '@/shared/parsers';
 import type { EventHandlerArgs } from './index';
 
-const eventArgs = z.intersection(
-  z.union([
-    z.object({ quoteAsset: chainflipAssetEnum }),
-    z
-      .object({ pairAsset: chainflipAssetEnum })
-      .transform(({ pairAsset }) => ({ quoteAsset: pairAsset })),
-  ]),
-  z.union([
-    z.object({
+const eventArgs = z.union([
+  z.object({
+    baseAsset: chainflipAssetEnum,
+    quoteAsset: chainflipAssetEnum,
+    feeHundredthPips: unsignedInteger,
+  }),
+  // support 1.0 event shape used on sisyphos
+  z
+    .object({
       baseAsset: chainflipAssetEnum,
+      pairAsset: chainflipAssetEnum,
       feeHundredthPips: unsignedInteger,
-    }),
-    // support previous event shape used on sisyphos 0.9
-    z
-      .object({
-        unstableAsset: chainflipAssetEnum,
-        feeHundredthPips: unsignedInteger,
-      })
-      .transform(({ unstableAsset, feeHundredthPips }) => ({
-        baseAsset: 'USDC' as const,
-        pairAsset: unstableAsset,
-        feeHundredthPips,
-      })),
-  ]),
-);
+    })
+    .transform(({ baseAsset, pairAsset, feeHundredthPips }) => ({
+      baseAsset,
+      quoteAsset: pairAsset,
+      feeHundredthPips,
+    })),
+  // support 0.9 event shape used on sisyphos
+  z
+    .object({
+      unstableAsset: chainflipAssetEnum,
+      feeHundredthPips: unsignedInteger,
+    })
+    .transform(({ unstableAsset, feeHundredthPips }) => ({
+      baseAsset: unstableAsset,
+      quoteAsset: 'USDC' as const,
+      feeHundredthPips,
+    })),
+]);
 
 export default async function newPoolCreated({
   prisma,
   event,
 }: EventHandlerArgs): Promise<void> {
-  const {
-    baseAsset,
-    quoteAsset: pairAsset,
-    feeHundredthPips,
-  } = eventArgs.parse(event.args);
+  const { baseAsset, quoteAsset, feeHundredthPips } = eventArgs.parse(
+    event.args,
+  );
+
+  // handle pools that were created with USDC as base asset on sisyphos: https://blocks.staging/events/384977-0
+  const stableAsset = baseAsset === 'USDC' ? baseAsset : quoteAsset;
+  const unstableAsset = baseAsset === 'USDC' ? quoteAsset : baseAsset;
 
   await prisma.pool.create({
     data: {
-      baseAsset,
-      pairAsset,
+      baseAsset: unstableAsset,
+      quoteAsset: stableAsset,
       liquidityFeeHundredthPips: Number(feeHundredthPips),
     },
   });
