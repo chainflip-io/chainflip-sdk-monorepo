8eb6f811a5ff7a152e1ccbd7ebad82ef00221767
Niklas Natter <nat@chainflip.io>
2023-12-07 17:32:56 +0100

    Support old event shape in poolFeeSet

--- packages/swap/src/event-handlers/poolFeeSet.ts
+++ packages/swap/src/event-handlers/poolFeeSet.ts
@@ -2,11 +2,25 @@ import { z } from 'zod';
 import { chainflipAssetEnum, unsignedInteger } from '@/shared/parsers';
 import type { EventHandlerArgs } from './index';
 
-const eventArgs = z.object({
-  baseAsset: chainflipAssetEnum,
-  quoteAsset: chainflipAssetEnum,
-  feeHundredthPips: unsignedInteger,
-});
+const eventArgs = z.union([
+  z.object({
+    baseAsset: chainflipAssetEnum,
+    quoteAsset: chainflipAssetEnum,
+    feeHundredthPips: unsignedInteger,
+  }),
+  // support 1.0 event shape used on perseverance
+  z
+    .object({
+      baseAsset: chainflipAssetEnum,
+      pairAsset: chainflipAssetEnum,
+      feeHundredthPips: unsignedInteger,
+    })
+    .transform(({ baseAsset, pairAsset, feeHundredthPips }) => ({
+      baseAsset,
+      quoteAsset: pairAsset,
+      feeHundredthPips,
+    })),
+]);
 
 export default async function poolFeeSet({
   prisma,
