824baff3512c5e50afc055954f550e84706f4e1b
Niklas Natter <nat@chainflip.io>
2023-12-07 17:05:15 +0100

    chore(swap): Always store unstable asset as base asset

--- packages/swap/src/event-handlers/poolFeeSet.ts
+++ packages/swap/src/event-handlers/poolFeeSet.ts
@@ -2,34 +2,25 @@ import { z } from 'zod';
 import { chainflipAssetEnum, unsignedInteger } from '@/shared/parsers';
 import type { EventHandlerArgs } from './index';
 
-const eventArgs = z.intersection(
-  z.object({
-    baseAsset: chainflipAssetEnum,
-    feeHundredthPips: unsignedInteger,
-  }),
-  z.union([
-    z.object({ quoteAsset: chainflipAssetEnum }),
-    z
-      .object({ pairAsset: chainflipAssetEnum })
-      .transform(({ pairAsset }) => ({ quoteAsset: pairAsset })),
-  ]),
-);
+const eventArgs = z.object({
+  baseAsset: chainflipAssetEnum,
+  quoteAsset: chainflipAssetEnum,
+  feeHundredthPips: unsignedInteger,
+});
 
 export default async function poolFeeSet({
   prisma,
   event,
 }: EventHandlerArgs): Promise<void> {
-  const {
-    baseAsset,
-    quoteAsset: pairAsset,
-    feeHundredthPips,
-  } = eventArgs.parse(event.args);
+  const { baseAsset, quoteAsset, feeHundredthPips } = eventArgs.parse(
+    event.args,
+  );
 
   await prisma.pool.update({
     where: {
       baseAsset_pairAsset: {
         baseAsset,
-        pairAsset,
+        pairAsset: quoteAsset,
       },
     },
     data: {
