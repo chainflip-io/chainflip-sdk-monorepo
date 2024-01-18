56022de19ddcd17ed9f8e4edf6e61e0888ab0596
Andrew Dibble <andrew@dibble.codes>
2024-01-18 11:49:37 +0100

    refactor(swap): handle API changes, fix lrange (#468)
    * refactor(swap): update key namespace
    
    * lrange, api changes
    
    * other api changes
    
    * fix lock

--- packages/shared/src/node-apis/__tests__/redis.test.ts
+++ packages/shared/src/node-apis/__tests__/redis.test.ts
@@ -47,16 +47,18 @@ describe(RedisClient, () => {
       );
       expect(broadcast).toBeNull();
       expect(mock).toHaveBeenCalledWith(
-        'confirmations:Bitcoin:tb1pdz3akc5wa2gr69v3x87tfg0ka597dxqvfl6zhqx4y202y63cgw0q3rgpm6',
+        'mempool:Bitcoin:tb1pdz3akc5wa2gr69v3x87tfg0ka597dxqvfl6zhqx4y202y63cgw0q3rgpm6',
       );
     });
 
     it('returns the tx if found', async () => {
-      const mock = jest
-        .mocked(Redis.prototype.get)
-        .mockResolvedValueOnce(
-          JSON.stringify({ confirmations: 4, value: 3.14, tx_hash: '1234' }),
-        );
+      const mock = jest.mocked(Redis.prototype.get).mockResolvedValueOnce(
+        JSON.stringify({
+          confirmations: 4,
+          value: '0x12b74280',
+          tx_hash: '1234',
+        }),
+      );
       const client = new RedisClient(url);
       const broadcast = await client.getMempoolTransaction(
         'Bitcoin',
@@ -64,60 +66,58 @@ describe(RedisClient, () => {
       );
       expect(broadcast).toEqual({
         confirmations: 4,
-        value: '314000000',
+        value: 314000000n,
         tx_hash: '0x1234',
       });
       expect(mock).toHaveBeenCalledWith(
-        'confirmations:Bitcoin:tb1pdz3akc5wa2gr69v3x87tfg0ka597dxqvfl6zhqx4y202y63cgw0q3rgpm6',
+        'mempool:Bitcoin:tb1pdz3akc5wa2gr69v3x87tfg0ka597dxqvfl6zhqx4y202y63cgw0q3rgpm6',
       );
     });
   });
 
   describe(RedisClient.prototype.getDeposits, () => {
     it('returns an empty array if no deposits are found', async () => {
-      const mock = jest.mocked(Redis.prototype.get).mockResolvedValueOnce(null);
+      const mock = jest
+        .mocked(Redis.prototype.lrange)
+        .mockResolvedValueOnce([]);
       const client = new RedisClient(url);
       const deposits = await client.getDeposits('Ethereum', 'ETH', '0x1234');
       expect(deposits).toEqual([]);
-      expect(mock).toHaveBeenCalledWith('deposit:Ethereum:0x1234');
+      expect(mock).toHaveBeenCalledWith('deposit:Ethereum:0x1234', 0, -1);
     });
 
     it('returns the deposits if found', async () => {
-      const mock = jest.mocked(Redis.prototype.get).mockResolvedValueOnce(
-        JSON.stringify([
-          {
-            amount: '0x8000',
-            asset: { chain: 'Ethereum', asset: 'ETH' },
-            deposit_chain_block_height: 1234,
-          },
-        ]),
-      );
+      const mock = jest.mocked(Redis.prototype.lrange).mockResolvedValueOnce([
+        JSON.stringify({
+          amount: '0x8000',
+          asset: 'ETH',
+          deposit_chain_block_height: 1234,
+        }),
+      ]);
       const client = new RedisClient(url);
       const deposits = await client.getDeposits('Ethereum', 'ETH', '0x1234');
       expect(deposits).toEqual([
         {
           amount: 0x8000n,
-          asset: { chain: 'Ethereum', asset: 'ETH' },
+          asset: 'ETH',
           deposit_chain_block_height: 1234,
         },
       ]);
-      expect(mock).toHaveBeenCalledWith('deposit:Ethereum:0x1234');
+      expect(mock).toHaveBeenCalledWith('deposit:Ethereum:0x1234', 0, -1);
     });
 
     it('filters out other assets for the same chain', async () => {
-      const mock = jest.mocked(Redis.prototype.get).mockResolvedValueOnce(
-        JSON.stringify([
-          {
-            amount: '0x8000',
-            asset: { chain: 'Ethereum', asset: 'FLIP' },
-            deposit_chain_block_height: 1234,
-          },
-        ]),
-      );
+      const mock = jest.mocked(Redis.prototype.lrange).mockResolvedValueOnce([
+        JSON.stringify({
+          amount: '0x8000',
+          asset: 'FLIP',
+          deposit_chain_block_height: 1234,
+        }),
+      ]);
       const client = new RedisClient(url);
       const deposits = await client.getDeposits('Ethereum', 'ETH', '0x1234');
       expect(deposits).toEqual([]);
-      expect(mock).toHaveBeenCalledWith('deposit:Ethereum:0x1234');
+      expect(mock).toHaveBeenCalledWith('deposit:Ethereum:0x1234', 0, -1);
     });
   });
 });
