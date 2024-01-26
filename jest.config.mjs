f1223f6f2f275c23c3f632e009a729f71c3f75ee
Zoheb Salam <zoheb@chainflip.io>
2024-01-26 16:22:29 +0100

    return failed swap from routes

--- jest.config.mjs
+++ jest.config.mjs
@@ -218,6 +218,8 @@ export default {
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['/node_modules/.pnpm/(?!(superjson))'],
 
+  prettierPath: null,
+
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
 
