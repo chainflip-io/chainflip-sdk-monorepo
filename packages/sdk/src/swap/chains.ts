5f4b1431266b52cb47038b40857c2b242e67e287
Hamed Afshar <hamedwise@gmail.com>
2024-03-04 10:10:45 +0100

    use factory

--- packages/sdk/src/swap/chains.ts
+++ packages/sdk/src/swap/chains.ts
@@ -1,46 +1,27 @@
-import { ChainflipNetwork, Chains, isTestnet } from '@/shared/enums';
+import { ChainflipNetwork, Chains, Chain, isTestnet } from '@/shared/enums';
 import { isNotNullish } from '@/shared/guards';
 import { ChainData } from './types';
 import { Environment } from '../rpc';
 
-export const ethereum: (
+type ChainFn = (
   network: ChainflipNetwork,
   env: Pick<Environment, 'ingressEgress'>,
-) => ChainData = (network, env) => ({
-  chain: Chains.Ethereum,
-  name: 'Ethereum',
-  isMainnet: !isTestnet(network),
-  requiredBlockConfirmations: isNotNullish(
-    env.ingressEgress.witnessSafetyMargins.Ethereum,
-  )
-    ? Number(env.ingressEgress.witnessSafetyMargins.Ethereum) + 1
-    : undefined,
-});
+) => ChainData;
 
-export const polkadot: (
-  network: ChainflipNetwork,
-  env: Pick<Environment, 'ingressEgress'>,
-) => ChainData = (network, env) => ({
-  chain: Chains.Polkadot,
-  name: 'Polkadot',
-  isMainnet: !isTestnet(network),
-  requiredBlockConfirmations: isNotNullish(
-    env.ingressEgress.witnessSafetyMargins.Polkadot,
-  )
-    ? Number(env.ingressEgress.witnessSafetyMargins.Polkadot) + 1
-    : undefined,
-});
+const chainFactory =
+  (chain: Chain): ChainFn =>
+  (network, env) =>
+    ({
+      chain,
+      name: Chains[chain],
+      isMainnet: !isTestnet(network),
+      requiredBlockConfirmations: isNotNullish(
+        env.ingressEgress.witnessSafetyMargins[Chains[chain]],
+      )
+        ? Number(env.ingressEgress.witnessSafetyMargins[Chains[chain]]) + 1
+        : undefined,
+    }) as ChainData;
 
-export const bitcoin: (
-  network: ChainflipNetwork,
-  env: Pick<Environment, 'ingressEgress'>,
-) => ChainData = (network, env) => ({
-  chain: Chains.Bitcoin,
-  name: 'Bitcoin',
-  isMainnet: !isTestnet(network),
-  requiredBlockConfirmations: isNotNullish(
-    env.ingressEgress.witnessSafetyMargins.Bitcoin,
-  )
-    ? Number(env.ingressEgress.witnessSafetyMargins.Bitcoin) + 1
-    : undefined,
-});
+export const ethereum = chainFactory(Chains.Ethereum);
+export const polkadot = chainFactory(Chains.Polkadot);
+export const bitcoin = chainFactory(Chains.Bitcoin);
