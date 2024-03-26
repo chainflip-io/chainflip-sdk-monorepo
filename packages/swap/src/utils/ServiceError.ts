a1bb2e5bccee65e0e647ab52bec406f5e77fed6c
Niklas Natter <nat@chainflip.io>
2024-03-26 16:19:23 +0100

    feat(swap): Add code to ServiceError class

--- packages/swap/src/utils/ServiceError.ts
+++ packages/swap/src/utils/ServiceError.ts
@@ -6,29 +6,31 @@ type IgnoredField =
   | 'stackTraceLimit';
 
 export default class ServiceError extends Error {
-  static badRequest(message: string): ServiceError {
-    return new ServiceError(message, 400);
+  static badRequest(code: string, message: string): ServiceError {
+    return new ServiceError(code, message, 400);
   }
 
-  static notFound(message = 'resource not found'): ServiceError {
-    return new ServiceError(message, 404);
+  static notFound(code: string, message = 'resource not found'): ServiceError {
+    return new ServiceError(code, message, 404);
   }
 
-  static internalError(message = 'internal error'): ServiceError {
-    return new ServiceError(message, 500);
+  static internalError(code: string, message = 'internal error'): ServiceError {
+    return new ServiceError(code, message, 500);
   }
 
   static assert(
     condition: unknown,
-    code: Exclude<keyof typeof ServiceError, IgnoredField>,
+    type: Exclude<keyof typeof ServiceError, IgnoredField>,
+    code: string,
     message: string,
   ): asserts condition {
-    if (!condition) throw ServiceError[code](message);
+    if (!condition) throw ServiceError[type](code, message);
   }
 
   constructor(
+    readonly code: string,
     message: string,
-    readonly code: number,
+    readonly httpCode: number,
   ) {
     super(message);
 
