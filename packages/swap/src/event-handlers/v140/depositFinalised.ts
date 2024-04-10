bb719ed5cd310a9b80fe68365d4de51526d4701c
legape <gabriel.buragev96@gmail.com>
2024-04-10 13:33:07 +0200

    chore(swap): handle deposit received event name change [WEB-1010] (#692)
    * chore(swap): handle deposit received event name change
    
    * fix names
    
    * add tests
    
    * rename

---
+++ packages/swap/src/event-handlers/v140/depositFinalised.ts
@@ -0,0 +1,7 @@
+import {
+  depositReceivedArgs as depositReceivedArgsV120,
+  networkDepositReceived as networkDepositReceivedV120,
+} from '../v120/networkDepositReceived';
+
+export const depositFinalisedArgs = depositReceivedArgsV120;
+export const depositFinalised = networkDepositReceivedV120;
