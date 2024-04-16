f7f2c09bdd3f5e971aea666fe66ce963932f0418
Andrew Dibble <andrew@chainflip.io>
2024-04-16 16:52:41 +0200

    refactor

--- packages/swap/src/quoting/Leg.ts
+++ packages/swap/src/quoting/Leg.ts
@@ -4,50 +4,47 @@ import { InternalAsset, getAssetAndChain } from '@/shared/enums';
 import { Leg as MarketMakerLeg } from './schemas';
 import { SwapRateArgs } from '../utils/statechain';
 
-type BaseAsset = Exclude<InternalAsset, 'Usdc'>;
-
 export default class Leg {
-  static of(baseAsset: InternalAsset, quoteAsset: InternalAsset, amount: bigint): Leg;
-  static of(baseAsset: InternalAsset, quoteAsset: InternalAsset, amount: bigint | null): Leg | null;
-  static of(baseAsset: InternalAsset, quoteAsset: InternalAsset, amount: bigint | null) {
+  static of(srcAsset: InternalAsset, destAsset: InternalAsset, amount: bigint): Leg;
+  static of(srcAsset: InternalAsset, destAsset: InternalAsset, amount: bigint | null): Leg | null;
+  static of(srcAsset: InternalAsset, destAsset: InternalAsset, amount: bigint | null) {
     if (amount === null) return null;
-    assert(baseAsset !== quoteAsset, 'baseAsset and quoteAsset must be different');
-    assert(baseAsset === 'Usdc' || quoteAsset === 'Usdc', 'one of the assets must be USDC');
-    return new Leg(baseAsset, quoteAsset, amount);
+    assert(srcAsset !== destAsset, 'srcAsset and destAsset must be different');
+    assert(srcAsset === 'Usdc' || destAsset === 'Usdc', 'one of the assets must be Usdc');
+    return new Leg(srcAsset, destAsset, amount);
   }
 
   private constructor(
-    private readonly baseAsset: InternalAsset,
-    private readonly quoteAsset: InternalAsset,
+    private readonly srcAsset: InternalAsset,
+    private readonly destAsset: InternalAsset,
     public amount: bigint,
   ) {}
 
   toPoolJSON(): SwapRateArgs {
     return {
       amount: this.amount,
-      srcAsset: this.baseAsset,
-      destAsset: this.quoteAsset,
+      srcAsset: this.srcAsset,
+      destAsset: this.destAsset,
     };
   }
 
   private getSide(): 'BUY' | 'SELL' {
-    return this.quoteAsset !== 'Usdc' ? 'BUY' : 'SELL';
+    return this.destAsset !== 'Usdc' ? 'BUY' : 'SELL';
   }
 
   toMarketMakerJSON(): MarketMakerLeg {
     const side = this.getSide();
-    let normalizedBaseAsset: BaseAsset;
-    const normalizedQuoteAsset = 'Usdc';
+    let baseAsset: Exclude<InternalAsset, 'Usdc'>;
 
-    if (this.quoteAsset !== 'Usdc') {
-      normalizedBaseAsset = this.quoteAsset;
-    } else if (this.baseAsset !== 'Usdc') {
-      normalizedBaseAsset = this.baseAsset;
+    if (this.destAsset !== 'Usdc') {
+      baseAsset = this.destAsset;
+    } else if (this.srcAsset !== 'Usdc') {
+      baseAsset = this.srcAsset;
     }
 
     return {
-      base_asset: getAssetAndChain(normalizedBaseAsset!),
-      quote_asset: getAssetAndChain(normalizedQuoteAsset),
+      base_asset: getAssetAndChain(baseAsset!),
+      quote_asset: getAssetAndChain('Usdc'),
       amount: this.amount.toString(),
       side,
     };
