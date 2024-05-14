555a7f9a36758a290aec3580f2dbdfb0f87a9519
GabrielBuragev <gabriel.buragev96@gmail.com>
2024-05-14 11:58:38 +0200

    rename var

--- packages/swap/src/utils/boost.ts
+++ packages/swap/src/utils/boost.ts
@@ -10,19 +10,19 @@ export const getBoostFeeBpsForAmount = async ({
 }): Promise<number | undefined> => {
   assetBoostPoolsDepth.sort((a, b) => (a.tier < b.tier ? -1 : 1));
 
-  let _amount = amount;
+  let remainingAmount = amount;
   let feeAmount = 0n;
 
   for (const poolDepth of assetBoostPoolsDepth) {
     const poolAvailableAmount = poolDepth.availableAmount;
 
-    const amountToBeUsedFromPool = bigintMin(_amount, poolAvailableAmount);
+    const amountToBeUsedFromPool = bigintMin(remainingAmount, poolAvailableAmount);
     feeAmount += getPipAmountFromAmount(amountToBeUsedFromPool, poolDepth.tier);
-    _amount -= amountToBeUsedFromPool;
+    remainingAmount -= amountToBeUsedFromPool;
   }
 
   // Not enough liquidity in the boost pools
-  if (_amount > 0) return undefined;
+  if (remainingAmount > 0) return undefined;
 
   return Number((feeAmount * BigInt(ONE_IN_PIP)) / BigInt(amount));
 };
