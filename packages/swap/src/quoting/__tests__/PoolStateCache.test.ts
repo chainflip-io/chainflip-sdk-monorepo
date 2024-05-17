adc3b8563dc0324adb63f753ff198b3f8362eda6
Andrew Dibble <andrew@chainflip.io>
2024-05-17 10:22:37 +0200

    fix test

--- packages/swap/src/quoting/__tests__/PoolStateCache.test.ts
+++ packages/swap/src/quoting/__tests__/PoolStateCache.test.ts
@@ -1,5 +1,11 @@
 import axios from 'axios';
-import { InternalAsset, InternalAssets, getInternalAsset } from '@/shared/enums';
+import {
+  InternalAsset,
+  InternalAssets,
+  assetConstants,
+  getAssetAndChain,
+  getInternalAsset,
+} from '@/shared/enums';
 import { deferredPromise } from '@/swap/utils/promise';
 import PoolStateCache from '../PoolStateCache';
 
@@ -50,8 +56,18 @@ describe(PoolStateCache, () => {
     resolveHashRequest(hash);
 
     postSpy.mockImplementation(async (url, body: any) => {
-      const asset = getInternalAsset(body.params[0]);
       const { method } = body;
+      if (method === 'cf_supported_assets') {
+        return {
+          data: {
+            result: (Object.keys(InternalAssets) as InternalAsset[]).map((a) =>
+              getAssetAndChain(a),
+            ),
+          },
+        };
+      }
+
+      const asset = getInternalAsset(body.params[0]);
       expect(body.params.at(-1)).toEqual(hash);
       const mock = mockPoolStates[asset];
 
