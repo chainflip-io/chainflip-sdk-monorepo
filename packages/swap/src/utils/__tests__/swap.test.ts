a06875bdae1b83b2c4ed8ecd0a1a7f5cce741bd8
GabrielBuragev <gabriel.buragev96@gmail.com>
2024-05-17 15:46:42 +0200

    fix tests

--- packages/swap/src/utils/__tests__/swap.test.ts
+++ packages/swap/src/utils/__tests__/swap.test.ts
@@ -21,7 +21,20 @@ describe(estimateSwapDuration, () => {
     ['Eth', 'Eth', 12 + 12 + 3 * 6 + 12] as const,
     ['Eth', 'Dot', 12 + 12 + 3 * 6 + 6] as const,
     ['Eth', 'Btc', 12 + 12 + 3 * 6 + 600] as const,
-  ])(`estimates time for swap from %s to %s`, async (fromChain, toChain, expected) => {
-    expect(await estimateSwapDuration(fromChain, toChain)).toStrictEqual(expected);
+  ])(`estimates time for normal swap from %s to %s`, async (srcAsset, destAsset, expected) => {
+    expect(await estimateSwapDuration({ srcAsset, destAsset })).toStrictEqual(expected);
+  });
+
+  it.each([
+    ['Btc', 'Btc', 600 + 3 * 6 + 600] as const,
+    ['Btc', 'Eth', 600 + 3 * 6 + 12] as const,
+    ['Btc', 'Dot', 600 + 3 * 6 + 6] as const,
+    ['Eth', 'Eth', 12 + 3 * 6 + 12] as const,
+    ['Eth', 'Dot', 12 + 3 * 6 + 6] as const,
+    ['Eth', 'Btc', 12 + 3 * 6 + 600] as const,
+  ])(`estimates time for boosted swap from %s to %s`, async (srcAsset, destAsset, expected) => {
+    expect(await estimateSwapDuration({ srcAsset, destAsset, boosted: true })).toStrictEqual(
+      expected,
+    );
   });
 });
