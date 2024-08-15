4e54283030d2f70b04ba6cdaca8d4ffd8390bedd
Andrew Dibble <andrew@chainflip.io>
2024-08-15 17:27:38 +0200

    bump package

--- packages/shared/src/rpc/index.ts
+++ packages/shared/src/rpc/index.ts
@@ -79,13 +79,10 @@ export const getRuntimeVersion = createRequest('state_getRuntimeVersion');
 export const getBlockHash = createRequest('chain_getBlockHash');
 
 export const getAllBoostPoolsDepth = transform(createRequest('cf_boost_pools_depth'), (result) =>
-  result
-    // TODO(solana): remove this
-    .filter((depth) => depth.chain !== 'Solana')
-    .map(({ chain, asset, ...rest }) => ({
-      asset: getInternalAsset({ chain, asset }),
-      ...rest,
-    })),
+  result.map(({ chain, asset, ...rest }) => ({
+    asset: getInternalAsset({ chain, asset }),
+    ...rest,
+  })),
 );
 
 export type BoostPoolsDepth = Awaited<ReturnType<typeof getAllBoostPoolsDepth>>;
