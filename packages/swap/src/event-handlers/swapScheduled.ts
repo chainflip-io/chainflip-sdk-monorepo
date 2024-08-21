47049cb530c86b370f722da7cf8704dea10cd7ec
Andrew Dibble <andrew@chainflip.io>
2024-08-21 17:28:54 +0200

    fix bugs

--- packages/swap/src/event-handlers/swapScheduled.ts
+++ packages/swap/src/event-handlers/swapScheduled.ts
@@ -2,28 +2,15 @@ import { swappingSwapScheduled as schema141 } from '@chainflip/processor/141/swa
 import { swappingSwapScheduled as schema150 } from '@chainflip/processor/150/swapping/swapScheduled';
 import { swappingSwapScheduled as schema160 } from '@chainflip/processor/160/swapping/swapScheduled';
 import { z } from 'zod';
+import { parseSpecNumber } from './common';
+import { getOriginInfo } from './swapRequested';
 import type { EventHandlerArgs } from '.';
 
-const transformOldShape = ({
-  swapId,
-  depositAmount,
-  swapType,
-  ...rest
-}: z.output<typeof schema141> | z.output<typeof schema150>) => ({
-  swapId,
-  swapRequestId: swapId,
-  inputAmount: depositAmount,
-  swapType: swapType.__kind,
-  ...rest,
-});
+const allSchemas = z.union([schema160, schema150, schema141]);
 
-const swapScheduledArgs = z.union([
-  schema160,
-  schema150.transform(transformOldShape),
-  schema141.transform(transformOldShape),
-]);
+export type SwapScheduledEvent = z.input<typeof allSchemas>;
 
-export type SwapScheduledEvent = z.input<typeof swapScheduledArgs>;
+const preRefactorSchema = z.union([schema150, schema141]);
 
 const swapTypeMap = {
   CcmGas: 'GAS',
@@ -38,13 +25,73 @@ export default async function swapScheduled({
   block,
   event,
 }: EventHandlerArgs): Promise<void> {
-  const { swapId, swapRequestId, inputAmount, swapType, ...rest } = swapScheduledArgs.parse(
-    event.args,
-  );
+  let swapId;
+  let swapRequestId;
+  let inputAmount;
+  let swapType;
 
-  const swapRequest = await prisma.swapRequest.findUniqueOrThrow({
-    where: { nativeId: swapRequestId },
-  });
+  const spec = parseSpecNumber(block.specId);
+
+  let swapRequest;
+  if (spec < 160) {
+    const parsed = preRefactorSchema.parse(event.args);
+
+    ({
+      swapId,
+      swapId: swapRequestId,
+      depositAmount: inputAmount,
+      swapType: { __kind: swapType },
+    } = parsed);
+
+    const {
+      sourceAsset,
+      destinationAddress,
+      destinationAsset,
+      origin,
+      brokerCommission,
+      brokerFee,
+    } = parsed;
+
+    const brokerFeeOrCommission = brokerFee ?? brokerCommission;
+
+    const { originType, depositTransactionRef, swapDepositChannelId } = await getOriginInfo(
+      prisma,
+      sourceAsset,
+      origin,
+    );
+
+    swapRequest = await prisma.swapRequest.create({
+      data: {
+        nativeId: swapRequestId,
+        originType,
+        depositTransactionRef,
+        swapDepositChannelId,
+        srcAsset: sourceAsset,
+        destAsset: destinationAsset,
+        depositAmount: inputAmount.toString(),
+        requestType: 'LEGACY_SWAP',
+        destAddress: destinationAddress.address,
+        swapRequestedAt: new Date(block.timestamp),
+        // this is changing soon anyway
+        fees:
+          brokerFeeOrCommission != null
+            ? {
+                create: {
+                  type: 'BROKER' as const,
+                  asset: sourceAsset,
+                  amount: brokerFeeOrCommission.toString(),
+                },
+              }
+            : undefined,
+      },
+    });
+  } else {
+    ({ swapId, swapRequestId, inputAmount, swapType } = schema160.parse(event.args));
+
+    swapRequest = await prisma.swapRequest.findUniqueOrThrow({
+      where: { nativeId: swapRequestId },
+    });
+  }
 
   await prisma.swap.create({
     data: {
@@ -56,16 +103,6 @@ export default async function swapScheduled({
       type: swapTypeMap[swapType],
       swapScheduledAt: new Date(block.timestamp),
       swapScheduledBlockIndex: `${block.height}-${event.indexInBlock}`,
-      fees:
-        'brokerFee' in rest && rest.brokerFee != null
-          ? {
-              create: {
-                type: 'BROKER' as const,
-                asset: swapRequest.srcAsset,
-                amount: rest.brokerFee.toString(),
-              },
-            }
-          : undefined,
     },
   });
 }
