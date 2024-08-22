5877da68f7725cbdd928ba85b4d56c477c2d0555
Andrew Dibble <andrew@chainflip.io>
2024-08-22 10:01:20 +0200

    refactor(sdk): publish version without axios

--- packages/sdk/src/swap/services/ApiService.ts
+++ packages/sdk/src/swap/services/ApiService.ts
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import type { QuoteQueryParams, QuoteQueryResponse } from '@/shared/schemas';
+import type { QuoteQueryResponse } from '@/shared/schemas';
 import { CF_SDK_VERSION_HEADERS } from '../consts';
 import { QuoteRequest, QuoteResponse, SwapStatusRequest, SwapStatusResponse } from '../types';
 
@@ -14,25 +13,22 @@ export const getQuote: BackendQuery<
   QuoteResponse
 > = async (baseUrl, quoteRequest, { signal }) => {
   const { brokerCommissionBps, ...returnedRequestData } = quoteRequest;
-  const params: QuoteQueryParams = {
-    amount: returnedRequestData.amount,
-    srcChain: returnedRequestData.srcChain,
-    srcAsset: returnedRequestData.srcAsset,
-    destChain: returnedRequestData.destChain,
-    destAsset: returnedRequestData.destAsset,
-    ...(brokerCommissionBps && {
-      brokerCommissionBps: String(brokerCommissionBps),
-    }),
-  };
 
-  const { data } = await axios.get<QuoteQueryResponse>('/quote', {
-    baseURL: baseUrl,
-    params,
-    signal,
-    headers: CF_SDK_VERSION_HEADERS,
-  });
+  const url = new URL('/quote', baseUrl);
+  url.searchParams.set('amount', returnedRequestData.amount);
+  url.searchParams.set('srcChain', returnedRequestData.srcChain);
+  url.searchParams.set('srcAsset', returnedRequestData.srcAsset);
+  url.searchParams.set('destChain', returnedRequestData.destChain);
+  url.searchParams.set('destAsset', returnedRequestData.destAsset);
+  if (brokerCommissionBps) {
+    url.searchParams.set('brokerCommissionBps', String(brokerCommissionBps));
+  }
 
-  return { ...returnedRequestData, quote: data };
+  const res = await fetch(url, { signal, headers: CF_SDK_VERSION_HEADERS });
+
+  if (!res.ok) throw new Error(`Failed to get quote: ${res.statusText}`);
+
+  return { ...returnedRequestData, quote: (await res.json()) as QuoteQueryResponse };
 };
 
 export const getStatus: BackendQuery<SwapStatusRequest, SwapStatusResponse> = async (
@@ -40,10 +36,12 @@ export const getStatus: BackendQuery<SwapStatusRequest, SwapStatusResponse> = as
   { id },
   { signal },
 ): Promise<SwapStatusResponse> => {
-  const { data } = await axios.get<SwapStatusResponse>(`/swaps/${id}`, {
-    baseURL: baseUrl,
+  const res = await fetch(new URL(`/swaps/${id}`, baseUrl), {
     signal,
     headers: CF_SDK_VERSION_HEADERS,
   });
-  return data;
+
+  if (!res.ok) throw new Error(`Failed to get quote: ${res.statusText}`);
+
+  return res.json();
 };
