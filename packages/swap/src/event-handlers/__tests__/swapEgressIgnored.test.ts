8a9565dcf4d30c8ebf3ed303d26aa5b96b1decfa
Andrew Dibble <andrew@chainflip.io>
2024-08-26 16:41:35 +0200

    fix tests

--- packages/swap/src/event-handlers/__tests__/swapEgressIgnored.test.ts
+++ packages/swap/src/event-handlers/__tests__/swapEgressIgnored.test.ts
@@ -1,8 +1,6 @@
-import { InternalAssets } from '@/shared/enums';
 import metadataMock from './metadata.json';
-import { DOT_ADDRESS, createDepositChannel, swapEgressIgnoredMock } from './utils';
+import { DOT_ADDRESS, swapEgressIgnoredMock } from './utils';
 import prisma from '../../client';
-import { Block, Event } from '../../processBlocks';
 import swapEgressIgnored from '../swapEgressIgnored';
 
 jest.mock('@/shared/rpc', () => ({
@@ -13,65 +11,42 @@ const { event, block } = swapEgressIgnoredMock;
 
 describe(swapEgressIgnored, () => {
   beforeEach(async () => {
-    await prisma.$queryRaw`TRUNCATE TABLE "SwapDepositChannel", "Swap", "Egress", "IgnoredEgress", "StateChainError" CASCADE`;
+    await prisma.$queryRaw`TRUNCATE TABLE "SwapDepositChannel", "Swap", "SwapRequest", "Egress", "IgnoredEgress", "StateChainError" CASCADE`;
   });
 
   it('adds an IgnoredEgress record for a egress ignored event', async () => {
     const { swapId } = event.args;
 
-    // store a new swap intent to initiate a new swap
-    const swapDepositChannel = await createDepositChannel({
-      swaps: {
-        create: {
-          nativeId: BigInt(swapId),
-          depositAmount: '10000000000',
-          swapInputAmount: '10000000000',
-          swapOutputAmount: '10000000000',
-          depositReceivedAt: new Date(block.timestamp - 12000),
-          depositReceivedBlockIndex: `${block.height - 100}-${event.indexInBlock}`,
-          swapExecutedAt: new Date(block.timestamp - 6000),
-          swapExecutedBlockIndex: `${block.height}-${event.indexInBlock}`,
-          srcAsset: InternalAssets.Eth,
-          destAsset: InternalAssets.Dot,
-          destAddress: DOT_ADDRESS,
-          type: 'SWAP',
-          swapScheduledAt: new Date(Date.now() - 12000),
-          swapScheduledBlockIndex: `${block.height}-${event.indexInBlock}`,
-        },
+    await prisma.swapRequest.create({
+      data: {
+        nativeId: BigInt(swapId),
+        srcAsset: 'Btc',
+        destAsset: 'Eth',
+        destAddress: DOT_ADDRESS,
+        depositAmount: '100000000',
+        depositReceivedAt: new Date('2024-08-06T00:00:00.000Z'),
+        depositReceivedBlockIndex: '1-1',
+        originType: 'VAULT',
+        requestType: 'LEGACY_SWAP',
+        swapRequestedAt: new Date('2024-08-06T00:00:00.000Z'),
       },
     });
 
-    await swapEgressIgnored({
-      block: block as Block,
-      event: event as Event,
-      prisma,
-    });
-
-    const swap = await prisma.swap.findFirstOrThrow({
-      where: { swapDepositChannelId: swapDepositChannel.id },
-      include: { ignoredEgress: true },
-    });
-
-    expect(swap).toMatchSnapshot({
-      id: expect.any(BigInt),
-      createdAt: expect.any(Date),
-      updatedAt: expect.any(Date),
-      swapDepositChannelId: expect.any(BigInt),
-      swapScheduledAt: expect.any(Date),
-      ignoredEgress: {
-        id: expect.any(BigInt),
-        swapId: expect.any(BigInt),
-        stateChainErrorId: expect.any(Number),
-        createdAt: expect.any(Date),
-      },
-    });
+    await swapEgressIgnored({ block, event, prisma });
 
     expect(
-      await prisma.stateChainError.findUniqueOrThrow({
-        where: { id: swap.ignoredEgress!.stateChainErrorId },
+      await prisma.swapRequest.findUniqueOrThrow({
+        where: { nativeId: BigInt(swapId) },
+        include: { ignoredEgress: true },
       }),
     ).toMatchSnapshot({
-      id: expect.any(Number),
+      id: expect.any(BigInt),
+      ignoredEgress: {
+        id: expect.any(BigInt),
+        swapRequestId: expect.any(BigInt),
+        createdAt: expect.any(Date),
+        stateChainErrorId: expect.any(Number),
+      },
     });
   });
 });
