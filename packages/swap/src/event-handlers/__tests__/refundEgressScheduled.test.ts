8407e4dae1e57c7a88d150ee2be254703764446a
Andrew Dibble <andrew@chainflip.io>
2024-08-26 16:55:48 +0200

    more tests

--- packages/swap/src/event-handlers/__tests__/refundEgressScheduled.test.ts
+++ packages/swap/src/event-handlers/__tests__/refundEgressScheduled.test.ts
@@ -1,5 +1,4 @@
-import { InternalAssets } from '@/shared/enums';
-import { DOT_ADDRESS, createDepositChannel, swapEgressScheduledMock } from './utils';
+import { DOT_ADDRESS, swapEgressScheduledMock } from './utils';
 import prisma from '../../client';
 import refundEgressScheduled from '../refundEgressScheduled';
 
@@ -11,71 +10,37 @@ describe(refundEgressScheduled, () => {
   });
 
   it('creates egress for refund on an existing swap', async () => {
-    const { swapId } = event.args;
+    const swapId = BigInt(event.args.swapId);
 
     // store a new swap intent to initiate a new swap
-    const swapDepositChannel = await createDepositChannel({
-      swaps: {
-        create: {
-          nativeId: BigInt(swapId),
-          depositAmount: '10000000000',
-          swapInputAmount: '10000000000',
-          swapOutputAmount: '10000000000',
-          depositReceivedAt: new Date(block.timestamp - 12000),
-          depositReceivedBlockIndex: `${block.height - 100}-${event.indexInBlock}`,
-          swapExecutedAt: new Date(block.timestamp - 6000),
-          swapExecutedBlockIndex: `${block.height}-${event.indexInBlock}`,
-          srcAsset: InternalAssets.Eth,
-          destAsset: InternalAssets.Dot,
-          destAddress: DOT_ADDRESS,
-          type: 'SWAP',
-          swapScheduledAt: new Date(Date.now() - 12000),
-          swapScheduledBlockIndex: `${block.height}-${event.indexInBlock}`,
-        },
+    await prisma.swapRequest.create({
+      data: {
+        nativeId: BigInt(swapId),
+        depositAmount: '10000000000',
+        depositReceivedAt: new Date(block.timestamp - 12000),
+        depositReceivedBlockIndex: `${block.height - 100}-${event.indexInBlock}`,
+        srcAsset: 'Eth',
+        destAsset: 'Dot',
+        destAddress: DOT_ADDRESS,
+        requestType: 'LEGACY_SWAP',
+        originType: 'VAULT',
+        swapRequestedAt: new Date(block.timestamp - 12000),
       },
     });
 
-    await prisma.$transaction((tx) =>
-      refundEgressScheduled({
-        block: block as any,
-        event: {
-          ...event,
-          args: {
-            asset: {
-              __kind: 'Eth',
-            },
-            swapId: '9876545',
-            egressId: [{ __kind: 'Ethereum' }, '1'] as const,
-            egressFee: '0x123',
-            amount: '0x1234567890',
-          },
-        },
-        prisma: tx,
-      }),
-    );
+    refundEgressScheduled({ block, event, prisma });
 
-    const swap = await prisma.swap.findFirstOrThrow({
-      where: { swapDepositChannelId: swapDepositChannel.id },
+    const swapRequest = await prisma.swapRequest.findFirstOrThrow({
+      where: { nativeId: swapId },
       include: {
-        refundEgress: {
-          select: {
-            amount: true,
-            scheduledAt: true,
-            chain: true,
-          },
-        },
-        fees: true,
+        refundEgress: { select: { amount: true, scheduledAt: true, chain: true } },
+        fees: { select: { amount: true, asset: true, type: true } },
       },
     });
 
-    expect(swap).toMatchSnapshot({
+    expect(swapRequest).toMatchSnapshot({
       id: expect.any(BigInt),
       refundEgressId: expect.any(BigInt),
-      createdAt: expect.any(Date),
-      updatedAt: expect.any(Date),
-      swapDepositChannelId: expect.any(BigInt),
-      swapScheduledAt: expect.any(Date),
-      fees: [{ id: expect.any(BigInt), swapId: expect.any(BigInt) }],
     });
   });
 });
