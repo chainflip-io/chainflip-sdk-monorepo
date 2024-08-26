326fa2e4fa6480173556d57fb07ae67baf422a63
GabrielBuragev <gabriel.buragev96@gmail.com>
2024-08-26 17:42:35 +0200

    bump solana package version

--- packages/sdk/tsup.config.mjs
+++ packages/sdk/tsup.config.mjs
@@ -1,11 +1,10 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
   treeshake: true,
   minify: false,
   dts: true,
-  external: [/^node:/],
+  external: [/^node:!(crypto)/],
   format: ['cjs', 'esm'],
   entry: {
     swap: 'src/swap/index.ts',
