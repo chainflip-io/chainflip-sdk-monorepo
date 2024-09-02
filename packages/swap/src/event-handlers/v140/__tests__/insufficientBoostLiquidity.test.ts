cfb3ad566b0dec6097dbfdf61d4bd9a528867762
GabrielBuragev <gabriel.buragev96@gmail.com>
2024-09-02 15:41:21 +0200

    fix: handle boostable lp channels

--- packages/swap/src/event-handlers/v140/__tests__/insufficientBoostLiquidity.test.ts
+++ packages/swap/src/event-handlers/v140/__tests__/insufficientBoostLiquidity.test.ts
@@ -65,4 +65,59 @@ describe('insufficientBoostLiquidity', () => {
       },
     ]);
   });
+
+  it('does nothing when no swap deposit channel was found', async () => {
+    await prisma.depositChannel.create({
+      data: {
+        srcChain: 'Bitcoin',
+        depositAddress: 'tb1pdz3akc5wa2gr69v3x87tfg0ka597dxqvfl6zhqx4y202y63cgw0q3rgpm6',
+        channelId: 1n,
+        issuedBlock: 0,
+        isSwapping: false,
+      },
+    });
+    const eventData = insufficientBoostLiquidityMock({
+      amountAttempted: '1000000',
+      channelId: '1',
+    }) as any;
+    const event = eventData.event as any;
+    const block = eventData.block as any;
+
+    await prisma.$transaction(async (txClient) => {
+      await insufficientBoostLiquidity({
+        prisma: txClient,
+        event,
+        block,
+      });
+    });
+
+    await expect(prisma.failedBoost.count()).resolves.toBe(0);
+  });
+
+  it('asserts that a channel exist', async () => {
+    const eventData = insufficientBoostLiquidityMock({
+      amountAttempted: '1000000',
+      channelId: '1',
+    }) as any;
+    const event = eventData.event as any;
+    const block = eventData.block as any;
+
+    await prisma.$transaction(async (txClient) => {
+      await expect(
+        insufficientBoostLiquidity({
+          prisma: txClient,
+          event,
+          block,
+        }),
+      ).rejects.toThrow(
+        `InsufficientBoostLiquidity: Deposit channel not found for asset Btc and channelId 1`,
+      );
+    });
+
+    const channel = await prisma.swapDepositChannel.findFirst({
+      where: { id: 1 },
+    });
+
+    expect(channel).toBeNull();
+  });
 });
--- packages/swap/src/event-handlers/v140/insufficientBoostLiquidity.ts
+++ packages/swap/src/event-handlers/v140/insufficientBoostLiquidity.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { assetConstants } from '@/shared/enums';
 import { internalAssetEnum, u128 } from '@/shared/parsers';
 import { EventHandlerArgs } from '..';
 
@@ -12,20 +13,38 @@ export const insufficientBoostLiquiditySchema = z.object({
 export const insufficientBoostLiquidity = async ({ prisma, event, block }: EventHandlerArgs) => {
   const { channelId, asset, amountAttempted } = insufficientBoostLiquiditySchema.parse(event.args);
 
-  const channel = await prisma.swapDepositChannel.findFirstOrThrow({
-    where: { channelId, srcAsset: asset },
-  });
-
-  await prisma.failedBoost.create({
-    data: {
-      amount: amountAttempted.toString(),
-      failedAtTimestamp: new Date(block.timestamp),
-      failedAtBlockIndex: `${block.height}-${event.indexInBlock}`,
-      swapDepositChannel: {
-        connect: {
-          id: channel.id,
-        },
-      },
+  const depositChannel = await prisma.depositChannel.findFirst({
+    where: { channelId, srcChain: assetConstants[asset].chain },
+    orderBy: {
+      issuedBlock: 'desc',
     },
   });
+
+  if (!depositChannel) {
+    throw new Error(
+      `InsufficientBoostLiquidity: Deposit channel not found for asset ${asset} and channelId ${channelId}`,
+    );
+  }
+
+  if (depositChannel.isSwapping) {
+    const swapDepositChannel = await prisma.swapDepositChannel.findFirstOrThrow({
+      where: { channelId, srcAsset: asset },
+      orderBy: {
+        issuedBlock: 'desc',
+      },
+    });
+
+    await prisma.failedBoost.create({
+      data: {
+        amount: amountAttempted.toString(),
+        failedAtTimestamp: new Date(block.timestamp),
+        failedAtBlockIndex: `${block.height}-${event.indexInBlock}`,
+        swapDepositChannel: {
+          connect: {
+            id: swapDepositChannel.id,
+          },
+        },
+      },
+    });
+  }
 };
