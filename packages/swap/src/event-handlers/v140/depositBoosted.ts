bfcdd1c877fbea0335b9e307675584691792032f
Niklas Natter <nat@chainflip.io>
2024-10-04 10:41:25 +0200

    fix(swap): Store transaction ref when deposit is boosted [WEB-1512]

--- packages/swap/src/event-handlers/v140/depositBoosted.ts
+++ packages/swap/src/event-handlers/v140/depositBoosted.ts
@@ -4,15 +4,23 @@ import { ethereumIngressEgressDepositBoosted } from '@chainflip/processor/160/et
 import { polkadotIngressEgressDepositBoosted } from '@chainflip/processor/160/polkadotIngressEgress/depositBoosted';
 import { solanaIngressEgressDepositBoosted } from '@chainflip/processor/160/solanaIngressEgress/depositBoosted';
 import { z } from 'zod';
+import { assetConstants } from '@/shared/enums';
 import { ONE_IN_PIP } from '@/shared/functions';
 import { SwapFeeType } from '@/swap/client';
+import { getDepositTxRef } from '@/swap/event-handlers/common';
 import { EventHandlerArgs } from '..';
+
+const normalizeSchema = <T>(obj: T) => ({
+  ...obj,
+  depositDetails: undefined,
+  blockHeight: undefined,
+});
 
 const arbitrumSchema = arbitrumIngressEgressDepositBoosted;
 const bitcoinSchema = bitcoinIngressEgressDepositBoosted;
 const ethereumSchema = ethereumIngressEgressDepositBoosted;
 const polkadotSchema = polkadotIngressEgressDepositBoosted;
-const solanaSchema = solanaIngressEgressDepositBoosted;
+const solanaSchema = solanaIngressEgressDepositBoosted.transform(normalizeSchema);
 
 const depositBoostedSchema = z.union([
   arbitrumSchema,
@@ -27,8 +35,18 @@ export type DepositBoostedArgs = z.input<typeof depositBoostedSchema>;
 // DepositBoosted event is emitted instead of DepositFinalised event in v140 due to boost
 // We need to update the depositAmount and store the ingress fee just like we do in the DepositFinalised event
 export const depositBoosted = async ({ prisma, event, block }: EventHandlerArgs) => {
-  const { asset, boostFee, action, ingressFee, amounts, prewitnessedDepositId } =
-    depositBoostedSchema.parse(event.args);
+  const {
+    asset,
+    boostFee,
+    action,
+    ingressFee,
+    amounts,
+    prewitnessedDepositId,
+    depositDetails,
+    blockHeight,
+  } = depositBoostedSchema.parse(event.args);
+
+  const txRef = getDepositTxRef(assetConstants[asset].chain, depositDetails, blockHeight);
 
   if (action.__kind === 'Swap') {
     const depositAmount = amounts.reduce((acc, [, amount]) => acc + amount, BigInt(0));
@@ -45,6 +63,7 @@ export const depositBoosted = async ({ prisma, event, block }: EventHandlerArgs)
     const data = {
       effectiveBoostFeeBps: Number(effectiveBoostFeeBps),
       prewitnessedDepositId,
+      depositTransactionRef: txRef,
       depositBoostedAt: new Date(block.timestamp),
       depositBoostedBlockIndex: `${block.height}-${event.indexInBlock}`,
       fees,
