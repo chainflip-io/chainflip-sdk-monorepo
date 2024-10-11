15607ee3f5ce5f66c6c6da276a44a3244f5084a3
Zoheb Salam <zoheb@chainflip.io>
2024-10-11 13:39:08 +0200

    add migration

--- packages/swap/prisma/migrations/20241010135457_add_dca_params_to_swap_request/migration.sql
+++ packages/swap/prisma/migrations/20241010135457_add_dca_params_to_swap_request/migration.sql
@@ -1,3 +1,11 @@
 -- AlterTable
 ALTER TABLE "public"."SwapRequest" ADD COLUMN     "chunkIntervalBlocks" INTEGER,
 ADD COLUMN     "numberOfChunks" INTEGER;
+
+-- backfill dca params into swap request
+UPDATE "SwapRequest" sr
+SET
+	"numberOfChunks" = sc."numberOfChunks",
+  "chunkIntervalBlocks" = sc."chunkIntervalBlocks"
+FROM "SwapDepositChannel" sc
+WHERE sr."swapDepositChannelId" = sc.id;
