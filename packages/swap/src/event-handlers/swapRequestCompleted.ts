6637003ffa7a0cbe2d835bdd60d0432ab372c367
Niklas Natter <nat@chainflip.io>
2024-10-30 14:49:13 +0100

    feat(swap): Add executedAt to Quote [WEB-1557] (#1056)
    * feat(swap): Add executedAt to Quote [WEB-1557]
    
    * Update snaphots

--- packages/swap/src/event-handlers/swapRequestCompleted.ts
+++ packages/swap/src/event-handlers/swapRequestCompleted.ts
@@ -52,6 +52,7 @@ export default async function swapRequestCompleted({
     await prisma.quote.update({
       where: { id: channelQuote.id },
       data: {
+        executedAt: new Date(block.timestamp),
         executedPrice,
         executedSlippagePercent,
         swapRequestId: swapRequest.id,
