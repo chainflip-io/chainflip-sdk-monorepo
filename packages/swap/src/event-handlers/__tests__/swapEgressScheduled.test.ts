1052ab0a3da157a526cb84ba8b72188801c25b00
Niklas Natter <nat@chainflip.io>
2025-01-09 09:26:00 +0100

    fix(swap): Support 1.8 changes for SwapEgressScheduled (#1143)
    * fix(swap): Support 1.8 changes for SwapEgressScheduled
    
    * Update deposit finalised handler
    
    * Update swap requested handler
    
    * Add refund event mock

--- packages/swap/src/event-handlers/__tests__/swapEgressScheduled.test.ts
+++ packages/swap/src/event-handlers/__tests__/swapEgressScheduled.test.ts
@@ -9,12 +9,12 @@ describe(swapEgressScheduled, () => {
     await prisma.$queryRaw`TRUNCATE TABLE "SwapDepositChannel", "Swap", "Egress", "SwapRequest" CASCADE`;
   });
 
-  it('adds an egress and fee to a swap request', async () => {
-    const swapId = BigInt(event.args.swapId);
+  it('adds an egress and fee to a swap request for 160 schema', async () => {
+    const swapRequestId = BigInt(event.args.swapRequestId);
 
     await prisma.swapRequest.create({
       data: {
-        nativeId: BigInt(swapId),
+        nativeId: swapRequestId,
         depositAmount: '10000000000',
         swapInputAmount: '10000000000',
         depositFinalisedAt: new Date(block.timestamp - 12000),
@@ -29,10 +29,30 @@ describe(swapEgressScheduled, () => {
       },
     });
 
-    await swapEgressScheduled({ block, event, prisma });
+    await swapEgressScheduled({
+      block,
+      event: {
+        ...event,
+        args: {
+          dispatchInfo: {
+            class: [null],
+            weight: '101978000',
+            paysFee: [null],
+          },
+          swapId: '9876545',
+          swapRequestId: '9876545',
+          fee: '1000000000',
+          egressFee: '1000000000',
+          asset: { __kind: 'Eth' },
+          amount: '10000000000',
+          egressId: [{ __kind: 'Ethereum' }, '1'] as const,
+        },
+      },
+      prisma,
+    });
 
     const swapRequest = await prisma.swapRequest.findFirstOrThrow({
-      where: { nativeId: swapId },
+      where: { nativeId: swapRequestId },
       include: {
         egress: true,
         fees: { select: { amount: true, asset: true, type: true } },
@@ -50,21 +70,65 @@ describe(swapEgressScheduled, () => {
     });
   });
 
-  it('uses gas asset for egress fee when CCM', async () => {
+  it('adds an egress and fee to a swap request', async () => {
+    const swapRequestId = BigInt(event.args.swapRequestId);
+
+    await prisma.swapRequest.create({
+      data: {
+        nativeId: swapRequestId,
+        depositAmount: '10000000000',
+        swapInputAmount: '10000000000',
+        depositFinalisedAt: new Date(block.timestamp - 12000),
+        depositFinalisedBlockIndex: `${block.height - 100}-${event.indexInBlock}`,
+        srcAsset: 'Eth',
+        destAsset: 'Dot',
+        destAddress: DOT_ADDRESS,
+        requestType: 'LEGACY_SWAP',
+        originType: 'VAULT',
+        swapRequestedAt: new Date(block.timestamp - 12000),
+        swapRequestedBlockIndex: '92-398',
+      },
+    });
+
+    await swapEgressScheduled({ block, event, prisma });
+
+    const swapRequest = await prisma.swapRequest.findFirstOrThrow({
+      where: { nativeId: swapRequestId },
+      include: {
+        egress: true,
+        fees: { select: { amount: true, asset: true, type: true } },
+      },
+    });
+
+    expect(swapRequest).toMatchSnapshot({
+      id: expect.any(BigInt),
+      egressId: expect.any(BigInt),
+      egress: {
+        id: expect.any(BigInt),
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+      },
+    });
+  });
+
+  it('uses gas asset for egress fee when CCM for 160 schema', async () => {
     const ccmEvent = {
       ...event,
       args: {
-        ...event.args,
+        swapRequestId: '9876545',
+        fee: '1000000000',
+        egressFee: '1000000000',
+        amount: '10000000000',
         asset: { __kind: 'ArbUsdc' },
         egressId: [{ __kind: 'Arbitrum' }, '1'] as const,
       },
     };
 
-    const swapId = BigInt(ccmEvent.args.swapId);
+    const swapRequestId = BigInt(ccmEvent.args.swapRequestId);
 
     await prisma.swapRequest.create({
       data: {
-        nativeId: BigInt(swapId),
+        nativeId: swapRequestId,
         depositAmount: '1000000',
         swapInputAmount: '1000000',
         depositFinalisedAt: new Date(block.timestamp - 12000),
@@ -82,7 +146,7 @@ describe(swapEgressScheduled, () => {
     await swapEgressScheduled({ block, event: ccmEvent, prisma });
 
     const swapRequest = await prisma.swapRequest.findFirstOrThrow({
-      where: { nativeId: swapId },
+      where: { nativeId: swapRequestId },
       include: {
         egress: true,
         fees: { select: { amount: true, asset: true, type: true } },
