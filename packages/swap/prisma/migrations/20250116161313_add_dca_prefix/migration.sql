c967edb305a7818e8348be1f7bd5b2e33bf3577d
Niklas Natter <nat@chainflip.io>
2025-01-16 18:03:57 +0100

    Adjust migration

--- packages/swap/prisma/migrations/20250116161313_add_dca_prefix/migration.sql
+++ packages/swap/prisma/migrations/20250116161313_add_dca_prefix/migration.sql
@@ -1,20 +1,7 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `chunkIntervalBlocks` on the `SwapDepositChannel` table. All the data in the column will be lost.
-  - You are about to drop the column `numberOfChunks` on the `SwapDepositChannel` table. All the data in the column will be lost.
-  - You are about to drop the column `chunkIntervalBlocks` on the `SwapRequest` table. All the data in the column will be lost.
-  - You are about to drop the column `numberOfChunks` on the `SwapRequest` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
-ALTER TABLE "public"."SwapDepositChannel" DROP COLUMN "chunkIntervalBlocks",
-DROP COLUMN "numberOfChunks",
-ADD COLUMN     "dcaChunkIntervalBlocks" INTEGER,
-ADD COLUMN     "dcaNumberOfChunks" INTEGER DEFAULT 1;
+ALTER TABLE "public"."SwapDepositChannel" RENAME COLUMN "chunkIntervalBlocks" TO "dcaChunkIntervalBlocks";
+ALTER TABLE "public"."SwapDepositChannel" RENAME COLUMN "numberOfChunks" TO "dcaNumberOfChunks";
 
 -- AlterTable
-ALTER TABLE "public"."SwapRequest" DROP COLUMN "chunkIntervalBlocks",
-DROP COLUMN "numberOfChunks",
-ADD COLUMN     "dcaChunkIntervalBlocks" INTEGER,
-ADD COLUMN     "dcaNumberOfChunks" INTEGER;
+ALTER TABLE "public"."SwapRequest" RENAME COLUMN "chunkIntervalBlocks" TO "dcaChunkIntervalBlocks";
+ALTER TABLE "public"."SwapRequest" RENAME COLUMN "numberOfChunks" TO "dcaNumberOfChunks";
