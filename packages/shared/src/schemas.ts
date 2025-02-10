58de08f5428dc0004d1aa8402c2a803275a44f59
Niklas Natter <nat@chainflip.io>
2025-02-10 18:31:54 +0100

    chore(sdk): Check quote type in requestDepositAddressV2 and encodeVaultSwapData [WEB-1676]

--- packages/shared/src/schemas.ts
+++ packages/shared/src/schemas.ts
@@ -170,6 +170,7 @@ export type BoostedQuoteDetails = {
 interface BaseQuoteDetails {
   srcAsset: AssetAndChain;
   destAsset: AssetAndChain;
+  isVaultSwap: boolean;
   depositAmount: string;
   intermediateAmount?: string;
   egressAmount: string;
