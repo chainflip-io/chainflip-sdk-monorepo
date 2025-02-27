85566fc11ee572ab86bec4a1b7e5a9c001b837a1
Andrew Dibble <andrew@dibble.codes>
2025-02-27 15:07:39 +0100

    fix(swap): handle padded versions (#1240)

--- packages/swap/src/utils/__tests__/function.test.ts
+++ packages/swap/src/utils/__tests__/function.test.ts
@@ -6,23 +6,23 @@ vi.mock('@/shared/rpc');
 
 describe(isAtLeastSpecVersion, () => {
   it('handles 1.7.10', async () => {
-    vi.mocked(rpc.getRuntimeVersion).mockResolvedValueOnce({ specVersion: 1710 } as any);
+    vi.mocked(rpc.getRuntimeVersion).mockResolvedValueOnce({ specVersion: 10710 } as any);
 
     expect(await isAtLeastSpecVersion('1.8.0')).toBe(false);
 
-    vi.mocked(rpc.getRuntimeVersion).mockResolvedValueOnce({ specVersion: 1710 } as any);
+    vi.mocked(rpc.getRuntimeVersion).mockResolvedValueOnce({ specVersion: 10710 } as any);
 
     expect(await isAtLeastSpecVersion('1.7.9')).toBe(true);
   });
 
   it('handles 1.7.9', async () => {
-    vi.mocked(rpc.getRuntimeVersion).mockResolvedValueOnce({ specVersion: 179 } as any);
+    vi.mocked(rpc.getRuntimeVersion).mockResolvedValueOnce({ specVersion: 10709 } as any);
 
     expect(await isAtLeastSpecVersion('1.8.0')).toBe(false);
   });
 
   it('handles 1.8.0', async () => {
-    vi.mocked(rpc.getRuntimeVersion).mockResolvedValueOnce({ specVersion: 180 } as any);
+    vi.mocked(rpc.getRuntimeVersion).mockResolvedValueOnce({ specVersion: 10800 } as any);
 
     expect(await isAtLeastSpecVersion('1.8.0')).toBe(true);
   });
