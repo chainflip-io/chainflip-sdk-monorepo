c30753ad6cfb1f12d0eaaa07fb2b7738d334df61
Niklas Natter <nat@chainflip.io>
2025-03-12 15:02:45 +0100

    Split hex regex

--- packages/swap/src/utils/transactionRef.ts
+++ packages/swap/src/utils/transactionRef.ts
@@ -1,19 +1,23 @@
 import { CHARSET as BASE58_CHARSET } from '@chainflip/utils/base58';
 import { Chain } from '@/shared/enums';
 
-const hexRegex = /^(0x)?[a-f0-9]+$/i;
+const hexNoPrefixRegex = /^[a-f0-9]+$/i;
+const hexWithPrefixRegex = /^0x[a-f0-9]+$/i;
 const base58Regex = new RegExp(`^[${BASE58_CHARSET}]+$`);
 const dotRegex = /^\d+-\d+$/;
 
 export const isTransactionRef = (txRef: string) =>
-  hexRegex.test(txRef) || base58Regex.test(txRef) || dotRegex.test(txRef);
+  hexNoPrefixRegex.test(txRef) ||
+  hexWithPrefixRegex.test(txRef) ||
+  base58Regex.test(txRef) ||
+  dotRegex.test(txRef);
 
 export const getTransactionRefChains = (txRef: string): Chain[] => {
-  if (hexRegex.test(txRef) && txRef.startsWith('0x')) {
-    return ['Ethereum', 'Arbitrum'];
-  }
-  if (hexRegex.test(txRef) && !txRef.startsWith('0x')) {
+  if (hexNoPrefixRegex.test(txRef)) {
     return ['Bitcoin'];
+  }
+  if (hexWithPrefixRegex.test(txRef)) {
+    return ['Ethereum', 'Arbitrum'];
   }
   if (base58Regex.test(txRef)) {
     return ['Solana'];
