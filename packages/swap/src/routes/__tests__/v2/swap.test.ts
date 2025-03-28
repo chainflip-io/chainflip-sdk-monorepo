b63639391c2740c8f5b5c1d22eafbcaa397b3ba0
Sukhovii Anastasiia <anastasia@chainflip.io>
2025-03-28 17:22:46 +0100

    aufgeräumt

--- packages/swap/src/routes/__tests__/v2/swap.test.ts
+++ packages/swap/src/routes/__tests__/v2/swap.test.ts
@@ -1468,6 +1468,41 @@ describe('server', () => {
       expect(body).toMatchSnapshot();
     });
 
+    it('retrieves a swap from a vault origin', async () => {
+      const txHash = '0xb2dcb9ce8d50f0ab869995fee8482bcf304ffcfe5681ca748f90e34c0ad7b241';
+
+      const requestedEvent = clone(swapEventMap['Swapping.SwapRequested']);
+      (requestedEvent.args as SwapRequestedArgs190) = {
+        ...requestedEvent.args,
+        origin: {
+          __kind: 'Vault',
+          txId: {
+            value: txHash,
+            __kind: 'Evm',
+          },
+          brokerId: '0x9059e6d854b769a505d01148af212bf8cb7f8469a7153edce8dcaedd9d299125',
+        } as any,
+        brokerFees: [
+          {
+            bps: 10,
+            account: '0x9e8d88ae895c9b37b2dead9757a3452f7c2299704d91ddfa444d87723f94fe0c',
+          },
+        ],
+      } as SwapRequestedArgs190;
+
+      await processEvents([
+        requestedEvent,
+        swapEventMap['Swapping.SwapScheduled'],
+        swapEventMap['Swapping.SwapExecuted'],
+      ]);
+
+      const { body, status } = await request(server).get(`/v2/swaps/${txHash}`);
+      expect(status).toBe(200);
+      const { swapId, ...rest } = body;
+
+      expect(rest).toMatchSnapshot();
+    });
+
     it(`retrieves a swap from a vault origin in ${StateV2.Completed}`, async () => {
       const txHash = '0xb2dcb9ce8d50f0ab869995fee8482bcf304ffcfe5681ca748f90e34c0ad7b241';
 
