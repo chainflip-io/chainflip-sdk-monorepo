e4bfb2141fba9f12fd502994dcb970b94ee030db
Andrew Dibble <andrew@chainflip.io>
2025-05-05 17:31:40 +0200

    backfill

--- packages/swap/src/processor.ts
+++ packages/swap/src/processor.ts
@@ -1,9 +1,13 @@
 import { inspect } from 'util';
+import backfillFallbacks from './backfillFallbacks.js';
 import processBlocks from './processBlocks.js';
 import logger from './utils/logger.js';
 
 // start
-const start = () => {
+const start = async () => {
+  await backfillFallbacks().catch((error) => {
+    logger.error('error backfilling fallbacks', { error: inspect(error) });
+  });
   processBlocks().catch((error) => {
     logger.error('error processing blocks', { error: inspect(error) });
     process.exit(1);
