5d012c2c3bbc312fdcaffc88de5b97f7f445f208
Andrew Dibble <andrew@chainflip.io>
2025-05-07 10:58:03 +0200

    refactor(swap): remove code

--- packages/swap/src/processor.ts
+++ packages/swap/src/processor.ts
@@ -1,13 +1,9 @@
 import { inspect } from 'util';
-import backfillFallbacks from './backfillFallbacks.js';
 import processBlocks from './processBlocks.js';
 import logger from './utils/logger.js';
 
 // start
 const start = async () => {
-  await backfillFallbacks().catch((error) => {
-    logger.error('error backfilling fallbacks', { error: inspect(error) });
-  });
   processBlocks().catch((error) => {
     logger.error('error processing blocks', { error: inspect(error) });
     process.exit(1);
