e76f952aedb2911639dd43d23075d6334a1d36ba
GabrielBuragev <gabriel.buragev96@gmail.com>
2025-05-15 16:40:05 +0200

    chore(swap): handle refund fee

--- packages/swap/src/event-handlers/swapping/__tests__/refundedOnChain.test.ts
+++ packages/swap/src/event-handlers/swapping/__tests__/refundedOnChain.test.ts
@@ -110,4 +110,119 @@ describe(refundedOnChain, () => {
     `,
     );
   });
+  it('calculates the refund fee', async () => {
+    await prisma.swapRequest.create({
+      data: {
+        srcAsset: 'Btc',
+        swapInputAmount: '100000000',
+        depositAmount: '100000000',
+        destAsset: 'Eth',
+        nativeId: 1n,
+        originType: 'ON_CHAIN',
+        requestType: 'REGULAR',
+        swapRequestedAt: new Date('2025-03-25T18:00:00Z'),
+        swapRequestedBlockIndex: '1-1',
+        totalBrokerCommissionBps: 0,
+        onChainSwapInfo: {
+          create: {
+            accountId: 'cFNwtr2mPhpUEB5AyJq38DqMKMkSdzaL9548hajN2DRTwh7Mq',
+          },
+        },
+        srcAddress: 'cFNwtr2mPhpUEB5AyJq38DqMKMkSdzaL9548hajN2DRTwh7Mq',
+        destAddress: 'cFNwtr2mPhpUEB5AyJq38DqMKMkSdzaL9548hajN2DRTwh7Mq',
+        fokRefundAddress: 'cFNwtr2mPhpUEB5AyJq38DqMKMkSdzaL9548hajN2DRTwh7Mq',
+      },
+    });
+
+    await refundedOnChain({
+      prisma,
+      event: {
+        args: check<SwappingRefundedOnChainArgs>({
+          swapRequestId: '1',
+          accountId: bytesToHex(
+            ss58.decode('cFNwtr2mPhpUEB5AyJq38DqMKMkSdzaL9548hajN2DRTwh7Mq').data,
+          ),
+          amount: '95000000',
+          asset: { __kind: 'Eth' },
+        }),
+      },
+      block: {},
+    } as unknown as EventHandlerArgs);
+
+    expect(
+      await prisma.swapRequest.findUnique({
+        where: { nativeId: 1n },
+        include: { onChainSwapInfo: true, fees: true },
+      }),
+    ).toMatchInlineSnapshot(
+      {
+        id: expect.any(BigInt),
+        onChainSwapInfo: {
+          id: expect.any(Number),
+          swapRequestId: expect.any(BigInt),
+        },
+        fees: [
+          {
+            swapRequestId: expect.any(BigInt),
+          },
+        ],
+      },
+      `
+      {
+        "ccmGasBudget": null,
+        "ccmMessage": null,
+        "completedAt": null,
+        "completedBlockIndex": null,
+        "dcaChunkIntervalBlocks": null,
+        "dcaNumberOfChunks": null,
+        "depositAmount": "100000000",
+        "depositBoostedAt": null,
+        "depositBoostedBlockIndex": null,
+        "depositFinalisedAt": null,
+        "depositFinalisedBlockIndex": null,
+        "depositTransactionRef": null,
+        "destAddress": "cFNwtr2mPhpUEB5AyJq38DqMKMkSdzaL9548hajN2DRTwh7Mq",
+        "destAsset": "Eth",
+        "effectiveBoostFeeBps": null,
+        "egressId": null,
+        "fallbackEgressId": null,
+        "fees": [
+          {
+            "amount": "5000000",
+            "asset": "Eth",
+            "id": Any<BigInt>,
+            "swapId": null,
+            "swapRequestId": Any<BigInt>,
+            "type": "REFUND",
+          },
+        ],
+        "fokMinPriceX128": null,
+        "fokRefundAddress": "cFNwtr2mPhpUEB5AyJq38DqMKMkSdzaL9548hajN2DRTwh7Mq",
+        "fokRetryDurationBlocks": null,
+        "id": Any<BigInt>,
+        "maxBoostFeeBps": 0,
+        "nativeId": 1n,
+        "onChainSwapInfo": {
+          "accountId": "cFNwtr2mPhpUEB5AyJq38DqMKMkSdzaL9548hajN2DRTwh7Mq",
+          "id": Any<Number>,
+          "outputAmount": null,
+          "refundAmount": "95000000",
+          "swapRequestId": Any<BigInt>,
+        },
+        "originType": "ON_CHAIN",
+        "prewitnessedDepositId": null,
+        "refundEgressId": null,
+        "requestType": "REGULAR",
+        "srcAddress": "cFNwtr2mPhpUEB5AyJq38DqMKMkSdzaL9548hajN2DRTwh7Mq",
+        "srcAsset": "Btc",
+        "swapDepositChannelId": null,
+        "swapInputAmount": "100000000",
+        "swapOutputAmount": null,
+        "swapRequestedAt": 2025-03-25T18:00:00.000Z,
+        "swapRequestedBlockIndex": "1-1",
+        "totalBrokerCommissionBps": 0,
+      }
+    `,
+    );
+  });
 });
--- packages/swap/src/event-handlers/swapping/refundEgressScheduled.ts
+++ packages/swap/src/event-handlers/swapping/refundEgressScheduled.ts
@@ -2,6 +2,7 @@ import { swappingRefundEgressScheduled as schema180 } from '@chainflip/processor
 import { swappingRefundEgressScheduled as schema190 } from '@chainflip/processor/190/swapping/refundEgressScheduled';
 import z from 'zod';
 import type { EventHandlerArgs } from '../index.js';
+import { Prisma } from '../../client.js';
 
 const eventArgs = z.union([schema190, schema180]);
 
@@ -16,9 +17,27 @@ export default async function refundEgressScheduled({
     swapRequestId,
     egressId: [chain, nativeId],
     egressFee: [egressFee, egressFeeAsset],
-    amount,
+    amount: egressAmount,
   } = eventArgs.parse(event.args);
 
+  const [aggregate, swapRequest] = await Promise.all([
+    prisma.swap.aggregate({
+      where: { swapRequest: { nativeId: swapRequestId }, swapExecutedBlockIndex: { not: null } },
+      _sum: { swapInputAmount: true },
+    }),
+    prisma.swapRequest.findUniqueOrThrow({
+      where: { nativeId: swapRequestId },
+      include: { fees: { where: { type: 'INGRESS' } } },
+    }),
+  ]);
+
+  const refundFee = swapRequest
+    .depositAmount!.minus(aggregate._sum.swapInputAmount ?? 0)
+    .minus(egressAmount?.toString() ?? 0)
+    .minus(egressFee?.toString() ?? 0)
+    .minus(swapRequest.fees.reduce((acc, fee) => acc.plus(fee.amount), new Prisma.Decimal(0)));
+
+  console.log(refundFee);
   await prisma.swapRequest.update({
     where: { nativeId: swapRequestId },
     data: {
@@ -26,17 +45,28 @@ export default async function refundEgressScheduled({
         create: {
           nativeId,
           chain,
-          amount: amount.toString(),
+          amount: egressAmount.toString(),
           scheduledAt: new Date(block.timestamp),
           scheduledBlockIndex: `${block.height}-${event.indexInBlock}`,
         },
       },
       fees: {
-        create: {
-          type: 'EGRESS',
-          asset: egressFeeAsset,
-          amount: egressFee.toString(),
-        },
+        create: [
+          {
+            type: 'EGRESS',
+            asset: egressFeeAsset,
+            amount: egressFee.toString(),
+          },
+          ...(refundFee.gt(0)
+            ? [
+                {
+                  type: 'REFUND' as const,
+                  asset: egressFeeAsset,
+                  amount: refundFee.toFixed(),
+                },
+              ]
+            : []),
+        ],
       },
     },
   });
