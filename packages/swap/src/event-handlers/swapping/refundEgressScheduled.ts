b080919aaca21cf4f8ebf92754a5905a9c1b6d9c
Sukhovii Anastasiia <anastasia@chainflip.io>
2025-06-16 18:29:52 +0200

    adjusted zod schema & returned old code

--- packages/swap/src/event-handlers/swapping/refundEgressScheduled.ts
+++ packages/swap/src/event-handlers/swapping/refundEgressScheduled.ts
@@ -1,9 +1,14 @@
 import { swappingRefundEgressScheduled as schema11000 } from '@chainflip/processor/11000/swapping/refundEgressScheduled';
 import { swappingRefundEgressScheduled as schema190 } from '@chainflip/processor/190/swapping/refundEgressScheduled';
 import z from 'zod';
+import { Prisma } from '../../client.js';
 import type { EventHandlerArgs } from '../index.js';
 
-const eventArgs = z.union([schema11000, schema190]);
+// @ts-expect-error: type instantiation is excessively deep and possibly infinite
+const eventArgs = z.union([
+  schema11000,
+  schema190.transform(({ ...args }) => ({ ...args, refundFee: undefined })),
+]);
 
 export type RefundEgressScheduledArgs = z.input<typeof eventArgs>;
 
@@ -16,9 +21,54 @@ export default async function refundEgressScheduled({
     swapRequestId,
     egressId: [chain, nativeId],
     egressFee: [egressFee, egressFeeAsset],
-    refundFee,
     amount: egressAmount,
+    refundFee: protocolRefundFee,
   } = eventArgs.parse(event.args);
+
+  // TODO: add test to cover both cases
+  let refundFeeAmount: string | null = null;
+
+  if (!protocolRefundFee) {
+    const [aggregate, swapRequest] = await Promise.all([
+      prisma.swap.aggregate({
+        where: { swapRequest: { nativeId: swapRequestId }, swapExecutedBlockIndex: { not: null } },
+        _sum: { swapInputAmount: true },
+      }),
+      prisma.swapRequest.findUniqueOrThrow({
+        where: { nativeId: swapRequestId },
+        include: { fees: { where: { type: 'INGRESS' } } },
+      }),
+    ]);
+
+    const refundFee = swapRequest.swapInputAmount
+      .minus(aggregate._sum.swapInputAmount ?? 0)
+      .minus(egressAmount?.toString() ?? 0)
+      .minus(egressFee?.toString() ?? 0)
+      .minus(swapRequest.fees.reduce((acc, fee) => acc.plus(fee.amount), new Prisma.Decimal(0)));
+
+    if (refundFee.gt(0)) {
+      refundFeeAmount = refundFee.toFixed();
+    }
+  } else {
+    refundFeeAmount = Number(protocolRefundFee).toFixed();
+  }
+
+  const fees = [
+    {
+      type: 'EGRESS' as const,
+      asset: egressFeeAsset,
+      amount: egressFee.toString(),
+    },
+    ...(refundFeeAmount
+      ? [
+          {
+            type: 'REFUND' as const,
+            asset: egressFeeAsset,
+            amount: refundFeeAmount,
+          },
+        ]
+      : []),
+  ];
 
   await prisma.swapRequest.update({
     where: { nativeId: swapRequestId },
@@ -33,22 +83,7 @@ export default async function refundEgressScheduled({
         },
       },
       fees: {
-        create: [
-          {
-            type: 'EGRESS',
-            asset: egressFeeAsset,
-            amount: egressFee.toString(),
-          },
-          ...(refundFee > 0n
-            ? [
-                {
-                  type: 'REFUND' as const,
-                  asset: egressFeeAsset,
-                  amount: refundFee.toString(), // check .toFixed()
-                },
-              ]
-            : []),
-        ],
+        create: fees,
       },
     },
   });
--- packages/swap/src/event-handlers/swapping/refundedOnChain.ts
+++ packages/swap/src/event-handlers/swapping/refundedOnChain.ts
@@ -3,12 +3,42 @@ import { swappingRefundedOnChain as schema190 } from '@chainflip/processor/190/s
 import z from 'zod';
 import { EventHandlerArgs } from '../index.js';
 
-const swappingRefundedOnChain = z.union([schema11000, schema190]);
+const swappingRefundedOnChain = z.union([
+  schema11000,
+  schema190.transform(({ ...args }) => ({ ...args, refundFee: undefined })),
+]);
 
 export type SwappingRefundedOnChainArgs = z.input<typeof swappingRefundedOnChain>;
 
 export default async function refundedOnChain({ prisma, event }: EventHandlerArgs) {
-  const { amount, swapRequestId, asset, refundFee } = swappingRefundedOnChain.parse(event.args);
+  const {
+    amount,
+    swapRequestId,
+    asset,
+    refundFee: protocolRefundFee,
+  } = swappingRefundedOnChain.parse(event.args);
+
+  let refundFeeAmount: string | null = null;
+
+  if (!protocolRefundFee) {
+    const [aggregate, swapRequest] = await Promise.all([
+      prisma.swap.aggregate({
+        where: { swapRequest: { nativeId: swapRequestId }, swapExecutedBlockIndex: { not: null } },
+        _sum: { swapInputAmount: true },
+      }),
+      prisma.swapRequest.findUniqueOrThrow({ where: { nativeId: swapRequestId } }),
+    ]);
+
+    const refundFee = swapRequest.swapInputAmount
+      .minus(aggregate._sum.swapInputAmount ?? 0)
+      .minus(amount.toString());
+
+    if (refundFee.gt(0)) {
+      refundFeeAmount = refundFee.toFixed();
+    }
+  } else {
+    refundFeeAmount = Number(protocolRefundFee).toFixed();
+  }
 
   await prisma.swapRequest.update({
     where: { nativeId: swapRequestId },
@@ -18,12 +48,12 @@ export default async function refundedOnChain({ prisma, event }: EventHandlerArg
           refundAmount: amount.toString(),
         },
       },
-      ...(refundFee > 0n && {
+      ...(refundFeeAmount && {
         fees: {
           create: {
             type: 'REFUND',
             asset,
-            amount: refundFee.toString(),
+            amount: refundFeeAmount,
           },
         },
       }),
