220d1626dca38af5d0d2bb771713980b84595871
Niklas Natter <nat@chainflip.io>
2025-07-02 14:03:23 +0200

    chore(swap): Include time of last bitcoin block when estimating swap duration [WEB-2365] (#1378)
    * chore(swap): Include time of last bitcoin block when estimating swap duration [WEB-2365]
    
    * Add tests
    
    * Add test comment
    
    * Remove round

--- packages/swap/src/utils/__tests__/swap.test.ts
+++ packages/swap/src/utils/__tests__/swap.test.ts
@@ -1,5 +1,6 @@
-import { describe, it, expect, beforeAll } from 'vitest';
+import { describe, it, expect, beforeAll, beforeEach } from 'vitest';
 import { environment, mockRpcResponse } from '@/shared/tests/fixtures.js';
+import prisma from '../../client.js';
 import { estimateSwapDuration } from '../swap.js';
 
 describe(estimateSwapDuration, () => {
@@ -13,6 +14,10 @@ describe(estimateSwapDuration, () => {
 
       throw new Error(`unexpected axios call to ${url}: ${JSON.stringify(data)}`);
     });
+  });
+
+  beforeEach(async () => {
+    await prisma.$queryRaw`TRUNCATE TABLE "ChainTracking" CASCADE`;
   });
 
   it.each([
@@ -41,5 +46,44 @@ describe(estimateSwapDuration, () => {
       durations: expected,
       total: expected.deposit + expected.swap + expected.egress,
     });
+  });
+
+  describe('uses the time of the last bitcoin block to estimate the bitcoin inclusion duration', async () => {
+    it('returns default duration if no chain tracking is found', async () => {
+      expect(
+        await estimateSwapDuration({ srcAsset: 'Btc', destAsset: 'Eth', boosted: true }),
+      ).toMatchObject({
+        durations: { deposit: 600 + 6, swap: 12, egress: 12 + 90 },
+      });
+    });
+
+    it.each([
+      [0, 600, true],
+      [60, 540, true],
+      [300, 300, true],
+      [540, 60, true],
+      [600, 60, true], // always estimate at least 1 minute
+      [800, 60, true], // always estimate at least 1 minute
+      [320, 300, true], // round up to the nearest minute
+      [350, 300, true], // round up to the nearest minute
+    ])(
+      `estimates time when last block is %s seconds old`,
+      async (lastBlockAgeSeconds, inclusionTimeSeconds, isBoosted) => {
+        await prisma.chainTracking.create({
+          data: {
+            chain: 'Bitcoin',
+            height: 1n,
+            eventWitnessedBlock: 1,
+            blockTrackedAt: new Date(Date.now() - lastBlockAgeSeconds * 1000),
+          },
+        });
+
+        expect(
+          await estimateSwapDuration({ srcAsset: 'Btc', destAsset: 'Eth', boosted: isBoosted }),
+        ).toMatchObject({
+          durations: { deposit: inclusionTimeSeconds + 6, swap: 12, egress: 12 + 90 },
+        });
+      },
+    );
   });
 });
