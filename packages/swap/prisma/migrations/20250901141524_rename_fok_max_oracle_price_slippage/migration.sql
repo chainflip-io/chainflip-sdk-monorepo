3f5efd866451c1033798df708d5c8e5d6852bddd
Zoheb Salam <zoheb@chainflip.io>
2025-09-01 16:16:02 +0200

    fix migration

--- packages/swap/prisma/migrations/20250901141524_rename_fok_max_oracle_price_slippage/migration.sql
+++ packages/swap/prisma/migrations/20250901141524_rename_fok_max_oracle_price_slippage/migration.sql
@@ -1,9 +1,14 @@
 /*
   Warnings:
 
+  - You are about to drop the column `fokMaxOraclePriceSlippage` on the `SwapDepositChannel` table. All the data in the column will be lost.
   - You are about to drop the column `fokMaxOraclePriceSlippage` on the `SwapRequest` table. All the data in the column will be lost.
 
 */
+-- AlterTable
+ALTER TABLE "public"."SwapDepositChannel" DROP COLUMN "fokMaxOraclePriceSlippage",
+ADD COLUMN     "fokMaxOraclePriceSlippageBps" INTEGER;
+
 -- AlterTable
 ALTER TABLE "public"."SwapRequest" DROP COLUMN "fokMaxOraclePriceSlippage",
 ADD COLUMN     "fokMaxOraclePriceSlippageBps" INTEGER;
