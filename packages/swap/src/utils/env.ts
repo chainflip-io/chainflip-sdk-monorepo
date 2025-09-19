26fb364470d7c500d3c3a9dcda584aaa25cebb04
Zoheb Salam <zoheb@chainflip.io>
2025-09-19 11:30:44 +0200

    remove from .env

--- packages/swap/src/utils/env.ts
+++ packages/swap/src/utils/env.ts
@@ -4,6 +4,8 @@ import env from '../config/env.js';
 
 export const isLocalnet = () => env.CHAINFLIP_NETWORK === 'backspin';
 
+const DISABLE_ASSET: ChainflipAsset = 'Dot';
+
 export const assertRouteEnabled = ({
   srcAsset,
   destAsset,
@@ -13,14 +15,16 @@ export const assertRouteEnabled = ({
 }) => {
   if (
     env.FULLY_DISABLED_INTERNAL_ASSETS.has(srcAsset) ||
-    env.DISABLED_DEPOSIT_INTERNAL_ASSETS.has(srcAsset)
+    env.DISABLED_DEPOSIT_INTERNAL_ASSETS.has(srcAsset) ||
+    DISABLE_ASSET === srcAsset
   ) {
     throw ServiceError.unavailable(`Asset ${srcAsset} is disabled`);
   }
 
   if (
     env.FULLY_DISABLED_INTERNAL_ASSETS.has(destAsset) ||
-    env.DISABLED_DESTINATION_INTERNAL_ASSETS.has(destAsset)
+    env.DISABLED_DESTINATION_INTERNAL_ASSETS.has(destAsset) ||
+    DISABLE_ASSET === destAsset
   ) {
     throw ServiceError.unavailable(`Asset ${destAsset} is disabled`);
   }
