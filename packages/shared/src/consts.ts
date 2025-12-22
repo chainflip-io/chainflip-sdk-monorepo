58d60e182827504f8fc5ec02332e0eeee872606d
nmammeri <nmammeri@gmail.com>
2025-12-22 15:49:49 +0100

    chore: code style fix

--- packages/shared/src/consts.ts
+++ packages/shared/src/consts.ts
@@ -104,7 +104,7 @@ export const chainflipAssetToPriceAssetMap: Record<ChainflipAsset, PriceAsset | 
   HubDot: null,
   HubUsdc: null,
   HubUsdt: null,
-  Wbtc: null
+  Wbtc: null,
 };
 
 // we probably dont need this anymore, but leaving it here for now just to be safe. yeet later
