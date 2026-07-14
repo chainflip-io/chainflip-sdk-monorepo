8c3eb382bf56cf8a16c59a98d00950cd87c648b8
legape <gabriel.buragev96@gmail.com>
2026-07-14 11:47:13 +0200

    feat: dockerize localnet environment [WEB-3629] (#1617)
    * feat: dockerize localnet environment
    
    * expose postgres & redis
    
    * allow network based env to be copied
    
    * stop ignoring dist
    
    * address comment

---
+++ docker/localnet/init-dbs/00-create-databases.sql
@@ -0,0 +1,9 @@
+-- Runs once on first Postgres container start (mounted into
+-- /docker-entrypoint-initdb.d). Creates the logical databases the SDK localnet needs:
+--   swap          -> the swap service's Prisma database
+--   squid_archive -> populated by substrate-ingest, served by the indexer-gateway
+--
+-- CREATE DATABASE has no IF NOT EXISTS; this file only runs when the data volume is
+-- empty. Wipe the volume (`down -v`) to re-run.
+CREATE DATABASE swap;
+CREATE DATABASE squid_archive;
