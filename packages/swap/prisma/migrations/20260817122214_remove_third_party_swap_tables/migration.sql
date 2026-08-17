8b89676ad901f070ab5b3154770fb3a5705c1195
legape <gabriel.buragev96@gmail.com>
2026-08-17 14:31:36 +0200

    chore(swap): remove third party swap related code (#1632)

---
+++ packages/swap/prisma/migrations/20260817122214_remove_third_party_swap_tables/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the `ThirdPartySwap` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "ThirdPartySwap";
+
+-- DropEnum
+DROP TYPE "ThirdPartyProtocol";
